6d095e42b4b7c9c88f0f449521c19a36057a807e
Menno Holtkamp <menno.holtkamp@gmail.com>
2014-04-04 16:19:10 +0200

    Support for filenames with 'special' characters
    A (local) filename that contains characters other than ASCII 32-126 like '/tmp/maïs.jpg' was processed as being binary data, since the [check for printable characters](http://www.php.net/manual/en/function.ctype-print.php) in isBinary() failed.
    
    In the [extended ASCII table](http://www.asciitable.com/), we can see that this excludes a lot of characters that 'can' occur in a filename.

--- src/Intervention/Image/Image.php
+++ src/Intervention/Image/Image.php
@@ -102,15 +102,15 @@ class Image
                 // image properties come from gd image resource
                 $this->initFromResource($source);
 
+            } elseif (is_file($source) || filter_var($source, FILTER_VALIDATE_URL)) {
+
+                // image will be read from file / fetched from url before init
+                $this->initFromString(file_get_contents($source));
+
             } elseif ($this->isBinary($source)) {
 
                 // image properties come from binary image string
                 $this->initFromString($source);
-
-            } elseif (filter_var($source, FILTER_VALIDATE_URL)) {
-
-                // image will be fetched from url before init
-                $this->initFromString(file_get_contents($source));
 
             } else {
 

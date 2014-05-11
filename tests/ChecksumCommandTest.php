51e9ef0349ebd66e8161c2c11b7b3d19972f534a
Oliver Vogel <oliver@olivervogel.net>
2014-05-11 16:38:47 +0200

    added checksumCommand

---
+++ tests/ChecksumCommandTest.php
@@ -0,0 +1,26 @@
+<?php
+
+use Intervention\Image\Commands\ChecksumCommand;
+
+class ExifCommandTest extends PHPUnit_Framework_TestCase
+{
+    public function tearDown()
+    {
+        Mockery::close();
+    }
+    
+    public function testExecute()
+    {
+        $size = Mockery::mock('Intervention\Image\Size', array(3, 3));
+        $color = array(0,0,0,1);
+        $resource = imagecreatefrompng(__DIR__.'/images/tile.png');
+        $image = Mockery::mock('Intervention\Image\Image');
+        $image->shouldReceive('getSize')->once()->andReturn($size);
+        $image->shouldReceive('pickColor')->times(9)->andReturn($color);
+        $command = new ChecksumCommand(array());
+        $result = $command->execute($image);
+        $this->assertTrue($result);
+        $this->assertTrue($command->hasOutput());
+        $this->assertEquals('ec9cbdb71be04e26b4a89333f20c273b', $command->getOutput());
+    }
+}

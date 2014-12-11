ab3b4750daf64d796ced89584df4d44c1a4c69ee
Oliver Vogel <oliver@olivervogel.net>
2014-12-11 18:52:04 +0100

    fixed bug with very long data url

--- src/Intervention/Image/AbstractDecoder.php
+++ src/Intervention/Image/AbstractDecoder.php
@@ -204,7 +204,7 @@ abstract class AbstractDecoder
      */
     private function decodeDataUrl($data_url)
     {
-        $pattern = "/^data:(?:image\/.+)(?:charset=\".+\")?;base64,(?P<data>.+)$/";
+        $pattern = "/^data:(?:image\/[a-zA-Z\-\.]+)(?:charset=\".+\")?;base64,(?P<data>.+)$/";
         preg_match($pattern, $data_url, $matches);
 
         if (is_array($matches) && array_key_exists('data', $matches)) {

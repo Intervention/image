7b99dd9ba41c60de78b7f332ae6c022bb3c9069b
Oliver Vogel <oliver@olivervogel.net>
2014-12-13 12:15:37 +0100

    disposal property

--- src/Intervention/Image/Frame.php
+++ src/Intervention/Image/Frame.php
@@ -4,6 +4,10 @@ namespace Intervention\Image;
 
 class Frame
 {
+    const DISPOSAL_METHOD_LEAVE = 1;
+    const DISPOSAL_METHOD_BACKGROUND = 2;
+    const DISPOSAL_METHOD_PREVIOUS = 3;
+
     /**
      * Image data
      *
@@ -19,14 +23,23 @@ class Frame
     public $delay;
 
     /**
+     * Disposal method
+     *
+     * @var integer
+     */
+    public $disposal;
+
+    /**
      * Create new frame instance
      *
      * @param mixed   $core
      * @param integer $delay
+     * @param integer $disposal
      */
-    public function __construct($core, $delay = 0)
+    public function __construct($core, $delay = 0, $disposal = DISPOSAL_METHOD_LEAVE)
     {
         $this->core = $core;
         $this->delay = $delay;
+        $this->disposal = $disposal;
     }
 }

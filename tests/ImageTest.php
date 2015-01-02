f9003fea9afa78157efd4ca3662a8519ee8b0edd
Oliver Vogel <oliver@olivervogel.net>
2015-01-02 14:13:09 +0100

    fix

--- tests/ImageTest.php
+++ tests/ImageTest.php
@@ -12,14 +12,14 @@ class ImageTest extends PHPUnit_Framework_TestCase
     public function testGetCore()
     {
         $image = $this->getTestImage();
-        $this->assertEquals('mock', $image->getCore());
+        $this->assertEquals('core', $image->getCore());
     }
 
     public function testCommandCall()
     {
         $image = $this->getTestImage();
         $result = $image->test(1, 2, 3);
-        $this->assertEquals('mock', $result);   
+        $this->assertEquals('result', $result);   
     }
 
     public function testEncode()
@@ -67,11 +67,13 @@ class ImageTest extends PHPUnit_Framework_TestCase
     {
         $size = Mockery::mock('\Intervention\Image\Size', array(800, 600));
         $driver = Mockery::mock('\Intervention\Image\AbstractDriver');
+        $container = Mockery::mock('\Intervention\Image\ContainerInterface');
+        $container->shouldReceive('getCore')->andReturn('core');
         $command = Mockery::mock('\Intervention\Image\Commands\AbstractCommand');
         $command->shouldReceive('hasOutput')->andReturn(true);
-        $command->shouldReceive('getOutput')->andReturn('mock');
+        $command->shouldReceive('getOutput')->andReturn('result');
         $driver->shouldReceive('executeCommand')->andReturn($command);
-        $image = new Image($driver, 'mock');
+        $image = new Image($driver, $container);
         $image->mime = 'image/png';
         $image->dirname = './tmp';
         $image->basename = 'foo.png';

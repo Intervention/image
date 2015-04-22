c10021588b7c7ff8708fae071e82b0cb717c1e74
Oliver Vogel <oliver@olivervogel.com>
2015-04-22 17:55:49 +0200

    bugfix

--- src/Intervention/Image/Gd/Commands/BackupCommand.php
+++ src/Intervention/Image/Gd/Commands/BackupCommand.php
@@ -19,6 +19,10 @@ class BackupCommand extends \Intervention\Image\Commands\AbstractCommand
         $clone = imagecreatetruecolor($size->width, $size->height);
         imagealphablending($clone, false);
         imagesavealpha($clone, true);
+        $transparency = imagecolorallocatealpha($clone, 0, 0, 0, 127);
+        imagefill($clone, 0, 0, $transparency);
+
+        // copy image to clone
         imagecopy($clone, $image->getCore(), 0, 0, 0, 0, $size->width, $size->height);
 
         $image->setBackup($clone, $backupName);

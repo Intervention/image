b333684543ba8dc1cdeffa97fc14883ce12bfd06
Oliver Vogel <oliver@olivervogel.com>
2015-11-20 16:55:32 +0100

    changed checksum calculation

--- src/Intervention/Image/Commands/ChecksumCommand.php
+++ src/Intervention/Image/Commands/ChecksumCommand.php
@@ -12,17 +12,7 @@ class ChecksumCommand extends AbstractCommand
      */
     public function execute($image)
     {
-        $colors = array();
-
-        $size = $image->getSize();
-
-        for ($x=0; $x <= ($size->width-1); $x++) {
-            for ($y=0; $y <= ($size->height-1); $y++) {
-                $colors[] = $image->pickColor($x, $y, 'array');
-            }
-        }
-
-        $this->setOutput(md5(serialize($colors)));
+        $this->setOutput(md5($image->encode()));
 
         return true;
     }
--- tests/ChecksumCommandTest.php
+++ tests/ChecksumCommandTest.php
@@ -11,16 +11,13 @@ class ChecksumCommandTest extends PHPUnit_Framework_TestCase
     
     public function testExecute()
     {
-        $size = Mockery::mock('Intervention\Image\Size', array(3, 3));
-        $color = array(0,0,0,1);
-        $resource = imagecreatefrompng(__DIR__.'/images/tile.png');
         $image = Mockery::mock('Intervention\Image\Image');
-        $image->shouldReceive('getSize')->once()->andReturn($size);
-        $image->shouldReceive('pickColor')->times(9)->andReturn($color);
+        $image->shouldReceive('encode')->once()->andReturn('encoded_image_data');
+
         $command = new ChecksumCommand(array());
         $result = $command->execute($image);
         $this->assertTrue($result);
         $this->assertTrue($command->hasOutput());
-        $this->assertEquals('ec9cbdb71be04e26b4a89333f20c273b', $command->getOutput());
+        $this->assertEquals(md5('encoded_image_data'), $command->getOutput());
     }
 }

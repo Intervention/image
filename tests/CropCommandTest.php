65590b9c3d98b91649f0fda475b288f17ef9d42f
Mark Gallagher <mark.gallagher@houseology.com>
2016-02-25 16:32:44 +0000

    mock needs to be lowercase

--- tests/CropCommandTest.php
+++ tests/CropCommandTest.php
@@ -24,7 +24,7 @@ class CropCommandTest extends PHPUnit_Framework_TestCase
     public function testImagick()
     {
         $imagick = Mockery::mock('Imagick');
-        $imagick->shouldReceive('extentImage')->with(100, 150, 10, 20)->andReturn(true);
+        $imagick->shouldReceive('extentimage')->with(100, 150, 10, 20)->andReturn(true);
         $imagick->shouldReceive('setimagepage')->with(0, 0, 0, 0)->once();
         $image = Mockery::mock('Intervention\Image\Image');
         $image->shouldReceive('getCore')->times(2)->andReturn($imagick);

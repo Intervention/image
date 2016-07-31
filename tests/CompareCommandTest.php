af27644a5dd953db0b7ce61fcf72c2f3f7f8637a
Andreas Heiberg <andreas@heiberg.io>
2016-07-31 20:08:10 +0100

    Add CompareCommandTest
    These test’s don’t use mocks. They test the score of the comparison. The scores are not consistent between Gd and Imagick though as they use different comparison algorithms. But they approximate each other.

--- tests/CompareCommandTest.php
+++ tests/CompareCommandTest.php
@@ -1,5 +1,6 @@
 <?php
 
+use Intervention\Image\ImageManager;
 use Intervention\Image\Gd\Commands\CompareCommand as CompareGd;
 use Intervention\Image\Imagick\Commands\CompareCommand as CompareImagick;
 
@@ -10,57 +11,105 @@ class CompareCommandTest extends PHPUnit_Framework_TestCase
         Mockery::close();
     }
 
-    public function testGd()
+    public function testGdStandardComparison()
     {
-        $resource = imagecreatefrompng(__DIR__.'/images/gradient.png');
-        $image = $this->getMockBuilder('Intervention\Image\Image')
-            ->disableOriginalClone()
-            ->getMock();
-        $image->expects($this->exactly(2))
-            ->method('getCore')
-            ->will($this->returnValue($resource));
-
-        $otherResource = imagecreatefrompng(__DIR__.'/images/trim.png');
-        $otherImage = $this->getMockBuilder('Intervention\Image\Image')
-            ->disableOriginalClone()
-            ->getMock();
-        $otherImage->expects($this->once())
-            ->method('getCore')
-            ->will($this->returnValue($otherResource));
+        $imageManager = new ImageManager(array('driver' => 'gd'));
+        $image = $imageManager->make(__DIR__.'/images/compare_old.jpg');
+        $otherImage = $imageManager->make(__DIR__.'/images/compare_new.jpg');
 
         $command = new CompareGd(array($otherImage));
         $result = $command->execute($image);
+        $comparison = $command->getOutput();
 
         $this->assertTrue($result);
         $this->assertTrue($command->hasOutput());
-        $this->assertInstanceOf('Intervention\Image\Comparison', $command->getOutput());
+        $this->assertInstanceOf('Intervention\Image\Comparison', $comparison);
+        $this->assertEquals(157, $comparison->getScore());
+        $this->assertInstanceOf('Intervention\Image\Image', $comparison->getDiffImage());
     }
 
-    public function testImagick()
+    public function testGdNoThresholdComparison()
     {
-        $resource = Mockery::mock('Imagick');
-        $resource->shouldReceive('getimageblob')->andReturn(file_get_contents(__DIR__.'/images/gradient.png'));
-        $resource->shouldReceive('compare')->andReturn([null, 0]);
-        $image = $this->getMockBuilder('Intervention\Image\Image')
-            ->disableOriginalClone()
-            ->getMock();
-        $image->expects($this->exactly(2))
-            ->method('getCore')
-            ->will($this->returnValue($resource));
+        $imageManager = new ImageManager(array('driver' => 'gd'));
+        $image = $imageManager->make(__DIR__.'/images/compare_old.jpg');
+        $otherImage = $imageManager->make(__DIR__.'/images/compare_new.jpg');
 
-        $otherResource = Mockery::mock('Imagick');
-        $otherImage = $this->getMockBuilder('Intervention\Image\Image')
-            ->disableOriginalClone()
-            ->getMock();
-        $otherImage->expects($this->once())
-            ->method('getCore')
-            ->will($this->returnValue($otherResource));
+        $command = new CompareGd(array($otherImage, 0));
+        $result = $command->execute($image);
+        $comparison = $command->getOutput();
+
+        $this->assertTrue($result);
+        $this->assertTrue($command->hasOutput());
+        $this->assertInstanceOf('Intervention\Image\Comparison', $comparison);
+        $this->assertEquals(22699, $comparison->getScore());
+        $this->assertInstanceOf('Intervention\Image\Image', $comparison->getDiffImage());
+    }
+
+    public function testGdResizeComparison()
+    {
+        $imageManager = new ImageManager(array('driver' => 'gd'));
+        $image = $imageManager->make(__DIR__.'/images/compare_old.jpg');
+        $otherImage = $imageManager->make(__DIR__.'/images/compare_small.jpg');
+
+        $command = new CompareGd(array($otherImage));
+        $result = $command->execute($image);
+        $comparison = $command->getOutput();
+
+        $this->assertTrue($result);
+        $this->assertTrue($command->hasOutput());
+        $this->assertInstanceOf('Intervention\Image\Comparison', $comparison);
+        $this->assertEquals(5057, $comparison->getScore());
+        $this->assertInstanceOf('Intervention\Image\Image', $comparison->getDiffImage());
+    }
+
+    public function testImagickStandardComparison()
+    {
+        $imageManager = new ImageManager(array('driver' => 'imagick'));
+        $image = $imageManager->make(__DIR__.'/images/compare_old.jpg');
+        $otherImage = $imageManager->make(__DIR__.'/images/compare_new.jpg');
 
         $command = new CompareImagick(array($otherImage));
         $result = $command->execute($image);
+        $comparison = $command->getOutput();
 
         $this->assertTrue($result);
         $this->assertTrue($command->hasOutput());
-        $this->assertInstanceOf('Intervention\Image\Comparison', $command->getOutput());
+        $this->assertInstanceOf('Intervention\Image\Comparison', $comparison);
+        $this->assertEquals(172.0, $comparison->getScore());
+        $this->assertInstanceOf('Intervention\Image\Image', $comparison->getDiffImage());
+    }
+
+    public function testImagickNoThresholdComparison()
+    {
+        $imageManager = new ImageManager(array('driver' => 'imagick'));
+        $image = $imageManager->make(__DIR__.'/images/compare_old.jpg');
+        $otherImage = $imageManager->make(__DIR__.'/images/compare_new.jpg');
+
+        $command = new CompareImagick(array($otherImage, 0));
+        $result = $command->execute($image);
+        $comparison = $command->getOutput();
+
+        $this->assertTrue($result);
+        $this->assertTrue($command->hasOutput());
+        $this->assertInstanceOf('Intervention\Image\Comparison', $comparison);
+        $this->assertEquals(22665.0, $comparison->getScore());
+        $this->assertInstanceOf('Intervention\Image\Image', $comparison->getDiffImage());
+    }
+
+    public function testImagickResizeComparison()
+    {
+        $imageManager = new ImageManager(array('driver' => 'imagick'));
+        $image = $imageManager->make(__DIR__.'/images/compare_old.jpg');
+        $otherImage = $imageManager->make(__DIR__.'/images/compare_small.jpg');
+
+        $command = new CompareImagick(array($otherImage));
+        $result = $command->execute($image);
+        $comparison = $command->getOutput();
+
+        $this->assertTrue($result);
+        $this->assertTrue($command->hasOutput());
+        $this->assertInstanceOf('Intervention\Image\Comparison', $comparison);
+        $this->assertEquals(9992.0, $comparison->getScore());
+        $this->assertInstanceOf('Intervention\Image\Image', $comparison->getDiffImage());
     }
 }

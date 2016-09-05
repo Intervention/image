60bf26a69d9ef23d592167e3dbff77fc6ff64b06
Oliver Vogel <oliver@olivervogel.com>
2016-09-05 19:48:26 +0200

    collection

---
+++ src/Intervention/Image/Collection.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Intervention\Image;
+
+class Collection
+{
+    /**
+     * The items of the collection.
+     *
+     * @var array
+     */
+    protected $items = array();
+
+    /**
+     * Create a collection.
+     *
+     * @param  array  $items
+     * @return void
+     */
+    public function __construct($items = array())
+    {
+        $this->items = $items;
+    }
+
+    /**
+     * Count items in collection
+     *
+     * @return integer
+     */
+    public function count()
+    {
+        return count($this->items);
+    }
+
+    /**
+     * Return first item in collection
+     *
+     * @return mixed
+     */
+    public function first()
+    {
+        return reset($this->items);
+    }
+
+    /**
+     * Returns last item in collection
+     *
+     * @return mixed
+     */
+    public function last()
+    {
+        return end($this->items);
+    }
+}
---
+++ tests/CollectionTest.php
@@ -0,0 +1,36 @@
+<?php
+
+use Intervention\Image\Collection;
+
+class CollectionTest extends PHPUnit_Framework_TestCase
+{
+    public function testConstructor()
+    {
+        $collection = new Collection(array('one', 'two', 'three'));
+        $this->assertInstanceOf('Intervention\Image\Collection', $collection);
+    }
+
+    public function testCount()
+    {
+        $collection = new Collection(array('one', 'two', 'three'));
+        $this->assertEquals(3, $collection->count());
+    }
+
+    public function testFirst()
+    {
+        $collection = new Collection(array('one', 'two', 'three'));
+        $this->assertEquals('one', $collection->first());
+
+        $collection = new Collection(array(array('foo', 'bar'), 'two', 'three'));
+        $this->assertEquals(array('foo', 'bar'), $collection->first());
+    }
+
+    public function testLast()
+    {
+        $collection = new Collection(array('one', 'two', 'three'));
+        $this->assertEquals('three', $collection->last());
+
+        $collection = new Collection(array('one', 'two', array('foo', 'bar')));
+        $this->assertEquals(array('foo', 'bar'), $collection->last());
+    }
+}

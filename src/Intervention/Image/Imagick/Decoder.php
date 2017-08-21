f1a92309ab250e94fb92302f5e08a4c674bb4bc0
Spell6inder <spell6inder@gmail.com>
2017-08-21 13:34:04 +0300

    Fix for issue: #607
    Fix for issue: #607 IMGTYPE_TRUECOLORMATTE -> IMGTYPE_TRUECOLOR
    imagick 3.4.1
    ImageMagick 7.0.3

--- src/Intervention/Image/Imagick/Decoder.php
+++ src/Intervention/Image/Imagick/Decoder.php
@@ -20,7 +20,7 @@ class Decoder extends \Intervention\Image\AbstractDecoder
 
             $core->setBackgroundColor(new \ImagickPixel('transparent'));
             $core->readImage($path);
-            $core->setImageType(defined('\Imagick::IMGTYPE_TRUECOLORALPHA') ? \Imagick::IMGTYPE_TRUECOLORALPHA : \Imagick::IMGTYPE_TRUECOLORMATTE);
+            $core->setImageType(defined('\Imagick::IMGTYPE_TRUECOLORALPHA') ? \Imagick::IMGTYPE_TRUECOLORALPHA : (defined('\Imagick::IMGTYPE_TRUECOLORMATTE') ? \Imagick::IMGTYPE_TRUECOLORMATTE : \Imagick::IMGTYPE_TRUECOLOR));
 
         } catch (\ImagickException $e) {
             throw new \Intervention\Image\Exception\NotReadableException(

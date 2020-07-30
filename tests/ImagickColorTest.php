c8454f1a706139f22fb2818188bac58123a8e84d
dontEatMe <lvlbgame@gmail.com>
2020-07-30 16:33:15 +0300

    increase accuracy on test

--- tests/ImagickColorTest.php
+++ tests/ImagickColorTest.php
@@ -277,22 +277,22 @@ class ImagickColorTest extends TestCase
         $c = new Color([255, 255, 255, 1]);
         $i = $c->getRgba();
         $this->assertInternalType('string', $i);
-        $this->assertEquals($i, 'rgba(255, 255, 255, 1.00)');
+        $this->assertEquals($i, 'rgba(255, 255, 255, 1.000)');
 
         $c = new Color([181, 55, 23, 0.5]);
         $i = $c->getRgba();
         $this->assertInternalType('string', $i);
-        $this->assertEquals($i, 'rgba(181, 55, 23, 0.50)');
+        $this->assertEquals($i, 'rgba(181, 55, 23, 0.500)');
 
         $c = new Color([0, 0, 0, 1]);
         $i = $c->getRgba();
         $this->assertInternalType('string', $i);
-        $this->assertEquals($i, 'rgba(0, 0, 0, 1.00)');
+        $this->assertEquals($i, 'rgba(0, 0, 0, 1.000)');
 
         $c = new Color([255, 255, 255, 0.5]);
         $i = $c->getRgba();
         $this->assertInternalType('string', $i);
-        $this->assertEquals($i, 'rgba(255, 255, 255, 0.50)');
+        $this->assertEquals($i, 'rgba(255, 255, 255, 0.500)');
     }
 
     public function testDiffers()

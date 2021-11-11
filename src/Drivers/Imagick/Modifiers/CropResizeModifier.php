989166aced34485654756870206303ff62608270
Oliver Vogel <oliver@olivervogel.com>
2021-11-11 14:25:23 +0000

    CropResizeModifier

---
+++ src/Drivers/Imagick/Modifiers/CropResizeModifier.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Intervention\Image\Drivers\Imagick\Modifiers;
+
+use Intervention\Image\Interfaces\FrameInterface;
+use Intervention\Image\Interfaces\ImageInterface;
+use Intervention\Image\Interfaces\ModifierInterface;
+use Intervention\Image\Interfaces\SizeInterface;
+
+class CropResizeModifier implements ModifierInterface
+{
+    protected $crop;
+    protected $resize;
+    protected $position;
+
+    public function __construct(SizeInterface $crop, SizeInterface $resize, string $position = 'top-left')
+    {
+        $this->crop = $crop;
+        $this->resize = $resize;
+        $this->position = $position;
+    }
+
+    public function apply(ImageInterface $image): ImageInterface
+    {
+        $shouldCrop = $this->crop != $image->getSize();
+
+        foreach ($image as $frame) {
+            if ($shouldCrop) {
+                $frame->getCore()->cropImage(
+                    $this->crop->getWidth(),
+                    $this->crop->getHeight(),
+                    $this->crop->getPivot()->getX(),
+                    $this->crop->getPivot()->getY()
+                );
+            }
+
+            $frame->getCore()->scaleImage(
+                $this->resize->getWidth(),
+                $this->resize->getHeight()
+            );
+        }
+
+        return $image;
+    }
+}

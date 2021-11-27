aaa31d81e78719bc33bffb9826ab9cb7db93a70d
Oliver Vogel <oliver@olivervogel.com>
2021-11-27 19:18:22 +0000

    Fix

--- tests/Drivers/Gd/Modifiers/ResizeModifierTest.php
+++ tests/Drivers/Gd/Modifiers/ResizeModifierTest.php
@@ -3,12 +3,12 @@
 namespace Intervention\Image\Tests\Drivers\Gd\Modifiers;
 
 use Intervention\Image\Drivers\Gd\Image;
-use Intervention\Image\Drivers\Gd\Modifiers\CropResizeModifier;
+use Intervention\Image\Drivers\Gd\Modifiers\ResizeModifier;
 use Intervention\Image\Geometry\Size;
 use Intervention\Image\Tests\TestCase;
 use Intervention\Image\Tests\Traits\CanCreateGdTestImage;
 
-class CropResizeModifierTest extends TestCase
+class ResizeModifierTest extends TestCase
 {
     use CanCreateGdTestImage;
 
@@ -17,8 +17,8 @@ class CropResizeModifierTest extends TestCase
         $image = $this->createTestImage('trim.png');
         $this->assertEquals(50, $image->width());
         $this->assertEquals(50, $image->height());
-        $image->modify(new CropResizeModifier(new Size(50, 50), new Size(30, 20)));
-        $this->assertEquals(30, $image->width());
-        $this->assertEquals(20, $image->height());
+        $image->modify(new ResizeModifier(new Size(300, 100)));
+        $this->assertEquals(300, $image->width());
+        $this->assertEquals(100, $image->height());
     }
 }

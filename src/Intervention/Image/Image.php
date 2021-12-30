318b34984e54cd8ed62263a06f6ce95bfd04e168
Rohan Sakhale <rohansakhale@gmail.com>
2021-12-30 17:12:19 +0530

    Return self object for chaining

--- src/Intervention/Image/Image.php
+++ src/Intervention/Image/Image.php
@@ -52,7 +52,7 @@ use Psr\Http\Message\StreamInterface;
  * @method \Intervention\Image\Image widen(int $width, \Closure $callback = null)                                                                                     Resizes the current image to new width, constraining aspect ratio. Pass an optional Closure callback as third parameter, to apply additional constraints like preventing possible upsizing.
  * @method StreamInterface           stream(string $format = null, int $quality = 90)                                                                                 Build PSR-7 compatible StreamInterface with current image in given format and quality.
  * @method ResponseInterface         psrResponse(string $format = null, int $quality = 90)                                                                            Build PSR-7 compatible ResponseInterface with current image in given format and quality.
- * @method void                      dpi(int $dpi = 72, int $units = null)
+ * @method \Intervention\Image\Image dpi(int $dpi = 72, int $units = null)
  */
 class Image extends File
 {
@@ -356,7 +356,7 @@ class Image extends File
      *
      * @param integer $dpi
      * @param integer $method
-     * @return void
+     * @return Intervention\Image\Image
      */
     public function dpi(int $dpi = 72, int $units = null)
     {
@@ -368,6 +368,8 @@ class Image extends File
         }
         $this->core->setImageUnits($units);
         $this->core->setImageResolution($dpi, $dpi);
+
+        return $this;
     }
 
     /**

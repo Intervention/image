9828c9310c3db0cec0d498e279499c3c0556d5ed
Oliver Vogel <oliver@olivervogel.com>
2022-02-19 09:03:08 +0100

    Removed unused code

--- src/Drivers/Gd/Image.php
+++ src/Drivers/Gd/Image.php
@@ -2,16 +2,9 @@
 
 namespace Intervention\Image\Drivers\Gd;
 
-use GdImage;
-use Intervention\Image\Collection;
 use Intervention\Image\Drivers\Abstract\AbstractImage;
-use Intervention\Image\Drivers\Gd\Frame;
-use Intervention\Image\Geometry\Resizer;
-use Intervention\Image\Geometry\Size;
 use Intervention\Image\Interfaces\ColorInterface;
-use Intervention\Image\Interfaces\EncoderInterface;
 use Intervention\Image\Interfaces\ImageInterface;
-use Intervention\Image\Interfaces\SizeInterface;
 use IteratorAggregate;
 
 class Image extends AbstractImage implements ImageInterface, IteratorAggregate
--- src/Drivers/Imagick/Image.php
+++ src/Drivers/Imagick/Image.php
@@ -2,14 +2,9 @@
 
 namespace Intervention\Image\Drivers\Imagick;
 
-use Imagick;
-use Intervention\Image\Collection;
 use Intervention\Image\Drivers\Abstract\AbstractImage;
-use Intervention\Image\Drivers\Imagick\Frame;
-use Intervention\Image\Geometry\Size;
 use Intervention\Image\Interfaces\ColorInterface;
 use Intervention\Image\Interfaces\ImageInterface;
-use Intervention\Image\Interfaces\SizeInterface;
 use IteratorAggregate;
 
 class Image extends AbstractImage implements ImageInterface, IteratorAggregate

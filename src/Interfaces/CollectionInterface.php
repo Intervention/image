dc3253feefc0a2fdf245639962fa9b837fa88fd8
Oliver Vogel <oliver@olivervogel.com>
2022-06-18 16:07:36 +0200

    Add default attribute to CollectionInterface

--- src/Interfaces/CollectionInterface.php
+++ src/Interfaces/CollectionInterface.php
@@ -5,7 +5,7 @@ namespace Intervention\Image\Interfaces;
 interface CollectionInterface
 {
     public function push($item): CollectionInterface;
-    public function get(int $key);
+    public function get(int $key, $default = null);
     public function has(int $key);
     public function first();
     public function last();

e1e1291fc51206d9be96d4f04c7905d165551d87
Oliver Vogel <oliver@olivervogel.com>
2022-06-26 09:20:26 +0200

    Add position to typographic text line

--- src/Typography/Line.php
+++ src/Typography/Line.php
@@ -3,21 +3,29 @@
 namespace Intervention\Image\Typography;
 
 use Intervention\Image\Interfaces\FontInterface;
+use Intervention\Image\Geometry\Point;
 
 class Line
 {
+    protected $position;
+
     public function __construct(protected string $text)
     {
-        //
+        $this->position = new Point();
     }
 
-    public function __toString(): string
+    public function getPosition(): Point
     {
-        return $this->text;
+        return $this->position;
     }
 
     public function width(FontInterface $font): int
     {
         return $font->getBoxSize($this->text)->width();
+    }
+
+    public function __toString(): string
+    {
+        return $this->text;
     }
 }

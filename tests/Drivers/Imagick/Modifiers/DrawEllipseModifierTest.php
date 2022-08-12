fe05a1e35d6e8b524050f2688f31e5276ddb2224
Oliver Vogel <oliver@olivervogel.com>
2022-08-12 17:44:59 +0200

    Add tests for DrawEllipseModifiers

---
+++ tests/Drivers/Imagick/Modifiers/DrawEllipseModifierTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Intervention\Image\Tests\Drivers\Imagick\Modifiers;
+
+use Intervention\Image\Drivers\Imagick\Modifiers\DrawEllipseModifier;
+use Intervention\Image\Geometry\Ellipse;
+use Intervention\Image\Geometry\Point;
+use Intervention\Image\Tests\TestCase;
+use Intervention\Image\Tests\Traits\CanCreateImagickTestImage;
+
+class DrawEllipseModifierTest extends TestCase
+{
+    use CanCreateImagickTestImage;
+
+    public function testApply(): void
+    {
+        $image = $this->createTestImage('trim.png');
+        $this->assertEquals('00aef0', $image->pickColor(14, 14)->toHex());
+        $drawable = new Ellipse(10, 10);
+        $drawable->background('b53717');
+        $image->modify(new DrawEllipseModifier(new Point(14, 14), $drawable));
+        $this->assertEquals('b53717', $image->pickColor(14, 14)->toHex());
+    }
+}

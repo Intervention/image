f73d1a25919352325ad1d724f2f9202912ec9a89
Oliver Vogel <oliver@olivervogel.com>
2023-10-21 15:47:08 +0200

    Add doc blocks

--- src/Colors/Rgb/Channels/Red.php
+++ src/Colors/Rgb/Channels/Red.php
@@ -9,31 +9,71 @@ class Red implements ColorChannelInterface
 {
     protected int $value;
 
+    /**
+     * Create and validate new instance
+     *
+     * @param  int $value
+     */
     public function __construct(int $value)
     {
         $this->value = $this->validate($value);
     }
 
+    /**
+     * Alias of value()
+     *
+     * @return int
+     */
+    public function toInt(): int
+    {
+        return $this->value;
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @see ColorChannelInterface::value()
+     */
     public function value(): int
     {
         return $this->value;
     }
 
+    /**
+     * {@inheritdoc}
+     *
+     * @see ColorChannelInterface::normalize()
+     */
     public function normalize($precision = 32): float
     {
         return round($this->value() / $this->max(), $precision);
     }
 
+    /**
+     * {@inheritdoc}
+     *
+     * @see ColorChannelInterface::min()
+     */
     public function min(): int
     {
         return 0;
     }
 
+    /**
+     * {@inheritdoc}
+     *
+     * @see ColorChannelInterface::max()
+     */
     public function max(): int
     {
         return 255;
     }
 
+    /**
+     * {@inheritdoc}
+     *
+     * @see ColorChannelInterface::validate()
+     */
     public function validate(mixed $value): mixed
     {
         if ($value < $this->min() || $value > $this->max()) {
@@ -43,11 +83,21 @@ class Red implements ColorChannelInterface
         return $value;
     }
 
+    /**
+     * {@inheritdoc}
+     *
+     * @see ColorChannelInterface::toString()
+     */
     public function toString(): string
     {
         return (string) $this->value();
     }
 
+    /**
+     * {@inheritdoc}
+     *
+     * @see ColorChannelInterface::__toString()
+     */
     public function __toString(): string
     {
         return $this->toString();

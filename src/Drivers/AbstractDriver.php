08ae0df4f0028cadac01882c0cbca862c5166568
Oliver Vogel <oliver@olivervogel.com>
2023-11-26 11:27:05 +0100

    Enable external resolvables

--- src/Drivers/AbstractDriver.php
+++ src/Drivers/AbstractDriver.php
@@ -2,14 +2,21 @@
 
 namespace Intervention\Image\Drivers;
 
+use Intervention\Image\Analyzers\AbstractAnalyzer;
+use Intervention\Image\Encoders\AbstractEncoder;
 use Intervention\Image\Exceptions\NotSupportedException;
 use Intervention\Image\Interfaces\DriverInterface;
+use Intervention\Image\Modifiers\AbstractModifier;
 use ReflectionClass;
 
 abstract class AbstractDriver implements DriverInterface
 {
     public function resolve(object $input): object
     {
+        if ($this->isExternal($input)) {
+            return $input;
+        }
+
         $driver_namespace = (new ReflectionClass($this))->getNamespaceName();
         $class_path = substr(get_class($input), strlen("Intervention\\Image\\"));
         $specialized = $driver_namespace . "\\" . $class_path;
@@ -21,5 +28,22 @@ abstract class AbstractDriver implements DriverInterface
         }
 
         return new $specialized($input, $this);
+    }
+
+    private function isExternal(object $input): bool
+    {
+        if ($input instanceof AbstractModifier) {
+            return false;
+        }
+
+        if ($input instanceof AbstractAnalyzer) {
+            return false;
+        }
+
+        if ($input instanceof AbstractEncoder) {
+            return false;
+        }
+
+        return true;
     }
 }

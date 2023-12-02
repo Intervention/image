84d2aaad76eee3d87e568a9587107195304feb9e
Oliver Vogel <oliver@olivervogel.com>
2023-12-02 16:07:27 +0100

    Add PadModifier tests

---
+++ tests/Drivers/Gd/Modifiers/PadModifierTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Intervention\Image\Tests\Drivers\Gd\Modifiers;
+
+use Intervention\Image\Modifiers\PadModifier;
+use Intervention\Image\Tests\TestCase;
+use Intervention\Image\Tests\Traits\CanCreateGdTestImage;
+
+/**
+ * @requires extension imagick
+ * @covers \Intervention\Image\Modifiers\PadModifier
+ */
+class PadModifierTest extends TestCase
+{
+    use CanCreateGdTestImage;
+
+    public function testModify(): void
+    {
+        $image = $this->createTestImage('blocks.png');
+        $this->assertEquals(640, $image->width());
+        $this->assertEquals(480, $image->height());
+        $image->modify(new PadModifier(200, 100, 'ff0'));
+        $this->assertEquals(200, $image->width());
+        $this->assertEquals(100, $image->height());
+        $this->assertColor(255, 255, 0, 255, $image->pickColor(0, 0));
+        $this->assertColor(255, 0, 255, 0, $image->pickColor(140, 10));
+        $this->assertColor(255, 255, 0, 255, $image->pickColor(175, 10));
+    }
+}

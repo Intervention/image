f17fe7bb54ec18d75588f527052069a6e9b5d720
Oliver Vogel <oliver@olivervogel.com>
2023-12-09 11:10:43 +0100

    Remove exception on GD driver's ProfileRemovalModifier

--- src/Drivers/Gd/Modifiers/ProfileRemovalModifier.php
+++ src/Drivers/Gd/Modifiers/ProfileRemovalModifier.php
@@ -3,15 +3,14 @@
 namespace Intervention\Image\Drivers\Gd\Modifiers;
 
 use Intervention\Image\Drivers\DriverSpecializedModifier;
-use Intervention\Image\Exceptions\NotSupportedException;
 use Intervention\Image\Interfaces\ImageInterface;
 
 class ProfileRemovalModifier extends DriverSpecializedModifier
 {
     public function apply(ImageInterface $image): ImageInterface
     {
-        throw new NotSupportedException(
-            'Color profiles are not supported by GD driver.'
-        );
+        // Color profiles are not supported by GD, so the decoded
+        // image is already free of profiles anyway.
+        return $image;
     }
 }

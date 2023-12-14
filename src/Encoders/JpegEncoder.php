c0a3ef85a0d114fe6bb3fe739c6fcf8b4108fc99
Oliver Vogel <oliver@olivervogel.com>
2023-12-14 16:21:59 +0100

    Adapt default encoder quality values

--- src/Encoders/JpegEncoder.php
+++ src/Encoders/JpegEncoder.php
@@ -4,7 +4,7 @@ namespace Intervention\Image\Encoders;
 
 class JpegEncoder extends AbstractEncoder
 {
-    public function __construct(public int $quality = 80)
+    public function __construct(public int $quality = 75)
     {
     }
 }

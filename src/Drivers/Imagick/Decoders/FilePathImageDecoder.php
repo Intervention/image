c9903717abbc5c8895c9062fb6ae8cc012d69327
Oliver Vogel <oliver@olivervogel.com>
2024-01-08 16:40:01 +0100

    Fix bug
    PHP enconters problems on some machines when is_file() in
    FilePathImageDecoders receive values that are longer than the
    maximum-path-length of the host.
    
    This fix checks if the input is in this maximum path length.

--- src/Drivers/Imagick/Decoders/FilePathImageDecoder.php
+++ src/Drivers/Imagick/Decoders/FilePathImageDecoder.php
@@ -16,6 +16,10 @@ class FilePathImageDecoder extends BinaryImageDecoder implements DecoderInterfac
             throw new DecoderException('Unable to decode input');
         }
 
+        if (strlen($input) > PHP_MAXPATHLEN) {
+            throw new DecoderException('Unable to decode input');
+        }
+
         try {
             if (!@is_file($input)) {
                 throw new DecoderException('Unable to decode input');
@@ -25,7 +29,7 @@ class FilePathImageDecoder extends BinaryImageDecoder implements DecoderInterfac
         }
 
         // decode image
-        $image =  parent::decode(file_get_contents($input));
+        $image = parent::decode(file_get_contents($input));
 
         // set file path on origin
         $image->origin()->setFilePath($input);

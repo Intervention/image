0dcb9af5758a985edadbd59a559d5ab5d549a8dd
Oliver Vogel <oliver@olivervogel.com>
2024-01-12 17:18:46 +0100

    Fix bug in FillModifier when filling with image with Imagick

--- src/Drivers/Imagick/Modifiers/FillModifier.php
+++ src/Drivers/Imagick/Modifiers/FillModifier.php
@@ -4,7 +4,10 @@ namespace Intervention\Image\Drivers\Imagick\Modifiers;
 
 use Imagick;
 use ImagickDraw;
+use ImagickDrawException;
+use ImagickException;
 use ImagickPixel;
+use ImagickPixelException;
 use Intervention\Image\Drivers\DriverSpecializedModifier;
 use Intervention\Image\Drivers\Imagick\Frame;
 use Intervention\Image\Interfaces\ImageInterface;
@@ -24,9 +27,7 @@ class FillModifier extends DriverSpecializedModifier
         $call .= is_a($filling, ImagickPixel::class) ? 'WithColor' : 'WithImage';
 
         foreach ($image as $frame) {
-            $frame->setNative(
-                call_user_func([$this, $call], $frame, $filling)
-            );
+            call_user_func([$this, $call], $frame, $filling);
         }
 
         return $image;
@@ -51,6 +52,14 @@ class FillModifier extends DriverSpecializedModifier
         };
     }
 
+    /**
+     * Modify given frame by flood filling with given color at the modifier's position
+     *
+     * @param Frame $frame
+     * @param ImagickPixel $pixel
+     * @return Imagick
+     * @throws ImagickException
+     */
     private function floodFillWithColor(Frame $frame, ImagickPixel $pixel): Imagick
     {
         $target = $frame->native()->getImagePixelColor(
@@ -71,6 +80,15 @@ class FillModifier extends DriverSpecializedModifier
         return $frame->native();
     }
 
+    /**
+     * Modify given frame by filling it completely with given color
+     *
+     * @param Frame $frame
+     * @param ImagickPixel $pixel
+     * @return Imagick
+     * @throws ImagickDrawException
+     * @throws ImagickException
+     */
     private function fillAllWithColor(Frame $frame, ImagickPixel $pixel): Imagick
     {
         $draw = new ImagickDraw();
@@ -88,33 +106,56 @@ class FillModifier extends DriverSpecializedModifier
         return $frame->native();
     }
 
-    private function floodFillWithImage(Frame $frame, Imagick $texture): Imagick
+    /**
+     * Modify given frame by flood filling it with given texture at the modifier's position
+     *
+     * @param Frame $frame
+     * @param Imagick $texture
+     * @return void
+     * @throws ImagickException
+     * @throws ImagickPixelException
+     */
+    private function floodFillWithImage(Frame $frame, Imagick $texture): void
     {
         // create tile
         $tile = clone $frame->native();
 
-        // mask away color at position
-        $tile->transparentPaintImage(
-            $tile->getImagePixelColor($this->position->x(), $this->position->y()),
-            0,
-            0,
-            false,
-        );
+        // get color at position
+        $targetColor = $tile->getImagePixelColor($this->position->x(), $this->position->y());
 
-        // create canvas
-        $canvas = clone $frame->native();
+        // mask away color at position
+        // does not work becaue there might be other transparent areas
+        $tile->transparentPaintImage($targetColor, 0, 0, false);
 
         // fill canvas with texture
-        $canvas = $canvas->textureImage($texture);
+        $canvas = $frame->native()->textureImage($texture);
 
         // merge canvas and tile
         $canvas->compositeImage($tile, Imagick::COMPOSITE_DEFAULT, 0, 0);
 
-        return $canvas;
+        // copy original alpha channel only if position is not completely transparent
+        if ($targetColor->getColorValue(Imagick::COLOR_ALPHA) != 0) {
+            $canvas->compositeImage($frame->native(), Imagick::COMPOSITE_DSTIN, 0, 0);
+        }
+
+        // replace imagick of frame
+        $frame->native()->compositeImage($canvas, Imagick::COMPOSITE_SRCOVER, 0, 0);
     }
 
-    private function fillAllWithImage(Frame $frame, Imagick $texture): Imagick
+    /**
+     * Fill given frame completely with given texture
+     *
+     * @param Frame $frame
+     * @param Imagick $texture
+     * @return void
+     * @throws ImagickException
+     */
+    private function fillAllWithImage(Frame $frame, Imagick $texture): void
     {
-        return $frame->native()->textureImage($texture);
+        // fill completely with texture
+        $modified = $frame->native()->textureImage($texture);
+
+        // replace imagick of frame
+        $frame->native()->compositeImage($modified, Imagick::COMPOSITE_SRCOVER, 0, 0);
     }
 }

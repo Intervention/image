23afc7fda29704c3c8eac349e66cac41f8d21a50
Oliver Vogel <oliver@olivervogel.com>
2024-01-15 17:18:00 +0100

    Correct typo

--- src/File.php
+++ src/File.php
@@ -42,7 +42,7 @@ class File implements FileInterface
             );
         }
 
-        // write date
+        // write data
         $saved = @file_put_contents($filepath, (string) $this);
         if ($saved === false) {
             throw new NotWritableException(

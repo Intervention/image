a5243592ac618a831f98921c0cadd1963e2ee429
Oliver Vogel <oliver@olivervogel.com>
2024-01-15 11:23:03 +0100

    Add Geometry traits tests

---
+++ tests/Geometry/Traits/HasBackgroundColor.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Intervention\Image\Tests\Geometry\Traits;
+
+use Intervention\Image\Geometry\Traits\HasBackgroundColor;
+use Intervention\Image\Tests\TestCase;
+
+class HasBackgroundColorTest extends TestCase
+{
+    public function getTestObject(): object
+    {
+        return new class () {
+            use HasBackgroundColor;
+        };
+    }
+
+    public function testSetGetBackgroundColor(): void
+    {
+        $object = $this->getTestObject();
+        $this->assertNull($object->backgroundColor());
+        $this->assertFalse($object->hasBackgroundColor());
+        $object->setBackgroundColor('fff');
+        $this->assertEquals('fff', $object->backgroundColor());
+        $this->assertTrue($object->hasBackgroundColor());
+    }
+}

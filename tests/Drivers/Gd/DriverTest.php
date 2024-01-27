f807ae818aa266fff9fe9f06c4c777e729fd601e
Oliver Vogel <oliver@olivervogel.com>
2024-01-27 13:17:38 +0100

    Add tests

--- tests/Drivers/Gd/DriverTest.php
+++ tests/Drivers/Gd/DriverTest.php
@@ -4,7 +4,11 @@ declare(strict_types=1);
 
 namespace Intervention\Image\Tests\Drivers\Gd;
 
+use Intervention\Image\Colors\Rgb\Colorspace;
+use Intervention\Image\Colors\Rgb\Decoders\HexColorDecoder;
 use Intervention\Image\Drivers\Gd\Driver;
+use Intervention\Image\Interfaces\ColorInterface;
+use Intervention\Image\Interfaces\ColorProcessorInterface;
 use Intervention\Image\Interfaces\ImageInterface;
 use Intervention\Image\Tests\TestCase;
 
@@ -42,5 +46,39 @@ class DriverTest extends TestCase
         $this->assertEquals(16, $image->height());
         $this->assertEquals(5, $image->loops());
         $this->assertEquals(2, $image->count());
+    }
+
+    public function testHandleInputImage(): void
+    {
+        $result = $this->driver->handleInput($this->getTestImagePath('test.jpg'));
+        $this->assertInstanceOf(ImageInterface::class, $result);
+    }
+
+    public function testHandleInputColor(): void
+    {
+        $result = $this->driver->handleInput('ffffff');
+        $this->assertInstanceOf(ColorInterface::class, $result);
+    }
+
+    public function testHandleInputObjects(): void
+    {
+        $result = $this->driver->handleInput('ffffff', [
+            new HexColorDecoder()
+        ]);
+        $this->assertInstanceOf(ColorInterface::class, $result);
+    }
+
+    public function testHandleInputClassnames(): void
+    {
+        $result = $this->driver->handleInput('ffffff', [
+            HexColorDecoder::class
+        ]);
+        $this->assertInstanceOf(ColorInterface::class, $result);
+    }
+
+    public function testColorProcessor(): void
+    {
+        $result = $this->driver->colorProcessor(new Colorspace());
+        $this->assertInstanceOf(ColorProcessorInterface::class, $result);
     }
 }

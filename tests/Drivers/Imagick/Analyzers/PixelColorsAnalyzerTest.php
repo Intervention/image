9015739860f4c09c974593d5e26b69c53bdaa718
Oliver Vogel <oliver@olivervogel.com>
2024-01-30 16:48:10 +0100

    Rename files to match namespaces & class names

---
+++ tests/Drivers/Imagick/Analyzers/PixelColorsAnalyzerTest.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Intervention\Image\Tests\Drivers\Imagick\Analyzers;
+
+use Intervention\Image\Analyzers\PixelColorsAnalyzer;
+use Intervention\Image\Collection;
+use Intervention\Image\Interfaces\ColorInterface;
+use Intervention\Image\Tests\TestCase;
+use Intervention\Image\Tests\Traits\CanCreateImagickTestImage;
+
+/**
+ * @requires extension imagick
+ * @covers \Intervention\Image\Analyzers\PixelColorsAnalyzer
+ * @covers \Intervention\Image\Drivers\Imagick\Analyzers\PixelColorsAnalyzer
+ */
+class PixelColorsAnalyzerTest extends TestCase
+{
+    use CanCreateImagickTestImage;
+
+    public function testAnalyze(): void
+    {
+        $image = $this->readTestImage('tile.png');
+        $analyzer = new PixelColorsAnalyzer(0, 0);
+        $result = $analyzer->analyze($image);
+        $this->assertInstanceOf(Collection::class, $result);
+        $this->assertInstanceOf(ColorInterface::class, $result->first());
+        $this->assertEquals('b4e000', $result->first()->toHex());
+    }
+}

0bf0890a8dce4ee28feb0556193812662cf35646
Oliver Vogel <oliver@olivervogel.com>
2024-03-21 16:11:45 +0100

    Remove redundant interface

--- src/Drivers/SpecializableDecoder.php
+++ src/Drivers/SpecializableDecoder.php
@@ -6,12 +6,11 @@ namespace Intervention\Image\Drivers;
 
 use Intervention\Image\Exceptions\DecoderException;
 use Intervention\Image\Interfaces\ColorInterface;
-use Intervention\Image\Interfaces\DecoderInterface;
 use Intervention\Image\Interfaces\ImageInterface;
 use Intervention\Image\Interfaces\SpecializableInterface;
 use Intervention\Image\Traits\CanBeDriverSpecialized;
 
-abstract class SpecializableDecoder extends AbstractDecoder implements DecoderInterface, SpecializableInterface
+abstract class SpecializableDecoder extends AbstractDecoder implements SpecializableInterface
 {
     use CanBeDriverSpecialized;
 

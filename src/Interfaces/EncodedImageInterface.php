968f9e498ea17e71bcc0bbfdc7645aad3cbc7342
Oliver Vogel <oliver@olivervogel.com>
2024-04-13 18:56:42 +0200

    Remove redundant code & double doc blocks

--- src/Interfaces/EncodedImageInterface.php
+++ src/Interfaces/EncodedImageInterface.php
@@ -21,45 +21,9 @@ interface EncodedImageInterface extends FileInterface
     public function mimetype(): string;
 
     /**
-     * Turn encoded image into DataUri format
+     * Transform encoded image data into an data uri string
      *
      * @return string
      */
     public function toDataUri(): string;
-
-    /**
-     * Save encoded image in filesystem
-     *
-     * @param string $filepath
-     * @return void
-     */
-    public function save(string $filepath): void;
-
-    /**
-     * Cast encoded image to string
-     *
-     * @return string
-     */
-    public function toString(): string;
-
-    /**
-     * Return file pointer of encoded image data
-     *
-     * @return resource
-     */
-    public function toFilePointer();
-
-    /**
-     * Return size in bytes of encoded image
-     *
-     * @return int
-     */
-    public function size(): int;
-
-    /**
-     * Cast encoded image data to string
-     *
-     * @return string
-     */
-    public function __toString(): string;
 }

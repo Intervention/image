33cbb217a00926149d96ed9e794100d8f8b53263
Oliver Vogel <oliver@olivervogel.com>
2024-04-23 16:17:12 +0200

    Filter out parameter that do not match target encoder (#1333)
    Filter out parameters/options that are passed to a format-unspecific encoder like AutoEncoder::class or MediaTypeEncoder::class but are not available in the format specific target encoder.

--- tests/Unit/Encoders/MediaTypeEncoderTest.php
+++ tests/Unit/Encoders/MediaTypeEncoderTest.php
@@ -6,82 +6,81 @@ namespace Intervention\Image\Tests\Unit\Encoders;
 
 use Intervention\Image\Encoders\AvifEncoder;
 use Intervention\Image\Encoders\BmpEncoder;
-use Intervention\Image\Encoders\FileExtensionEncoder;
 use Intervention\Image\Encoders\GifEncoder;
 use Intervention\Image\Encoders\HeicEncoder;
 use Intervention\Image\Encoders\Jpeg2000Encoder;
 use Intervention\Image\Encoders\JpegEncoder;
+use Intervention\Image\Encoders\MediaTypeEncoder;
 use Intervention\Image\Encoders\PngEncoder;
 use Intervention\Image\Encoders\TiffEncoder;
 use Intervention\Image\Encoders\WebpEncoder;
 use Intervention\Image\Exceptions\EncoderException;
-use Intervention\Image\FileExtension;
 use Intervention\Image\Interfaces\EncoderInterface;
+use Intervention\Image\MediaType;
 use Intervention\Image\Tests\BaseTestCase;
 use PHPUnit\Framework\Attributes\DataProvider;
 
-final class FileExtensionEncoderTest extends BaseTestCase
+final class MediaTypeEncoderTest extends BaseTestCase
 {
-    private function testEncoder(string|FileExtension $extension, array $options = []): EncoderInterface
+    private function testEncoder(string|MediaType $mediaType, array $options = []): EncoderInterface
     {
-        $encoder = new class ($extension, ...$options) extends FileExtensionEncoder
+        $encoder = new class ($mediaType, ...$options) extends MediaTypeEncoder
         {
             public function __construct($mediaType, ...$options)
             {
                 parent::__construct($mediaType, ...$options);
             }
 
-            public function test($extension)
+            public function test($mediaType)
             {
-                return $this->encoderByFileExtension($extension);
+                return $this->encoderByMediaType($mediaType);
             }
         };
 
-        return $encoder->test($extension);
+        return $encoder->test($mediaType);
     }
 
     #[DataProvider('targetEncoderProvider')]
-    public function testEncoderByFileExtensionString(
-        string|FileExtension $fileExtension,
+    public function testEncoderByMediaType(
+        string|MediaType $mediaType,
         string $targetEncoderClassname,
     ): void {
         $this->assertInstanceOf(
             $targetEncoderClassname,
-            $this->testEncoder($fileExtension),
+            $this->testEncoder($mediaType)
         );
     }
 
     public static function targetEncoderProvider(): array
     {
         return [
-            ['webp', WebpEncoder::class],
-            ['avif', AvifEncoder::class],
-            ['jpeg', JpegEncoder::class],
-            ['jpg', JpegEncoder::class],
-            ['bmp', BmpEncoder::class],
-            ['gif', GifEncoder::class],
-            ['png', PngEncoder::class],
-            ['tiff', TiffEncoder::class],
-            ['tif', TiffEncoder::class],
-            ['jp2', Jpeg2000Encoder::class],
-            ['heic', HeicEncoder::class],
-            [FileExtension::WEBP, WebpEncoder::class],
-            [FileExtension::AVIF, AvifEncoder::class],
-            [FileExtension::JPG, JpegEncoder::class],
-            [FileExtension::BMP, BmpEncoder::class],
-            [FileExtension::GIF, GifEncoder::class],
-            [FileExtension::PNG, PngEncoder::class],
-            [FileExtension::TIF, TiffEncoder::class],
-            [FileExtension::TIFF, TiffEncoder::class],
-            [FileExtension::JP2, Jpeg2000Encoder::class],
-            [FileExtension::HEIC, HeicEncoder::class],
+            ['image/webp', WebpEncoder::class],
+            ['image/avif', AvifEncoder::class],
+            ['image/jpeg', JpegEncoder::class],
+            ['image/bmp', BmpEncoder::class],
+            ['image/gif', GifEncoder::class],
+            ['image/png', PngEncoder::class],
+            ['image/png', PngEncoder::class],
+            ['image/tiff', TiffEncoder::class],
+            ['image/jp2', Jpeg2000Encoder::class],
+            ['image/heic', HeicEncoder::class],
+            [MediaType::IMAGE_WEBP, WebpEncoder::class],
+            [MediaType::IMAGE_AVIF, AvifEncoder::class],
+            [MediaType::IMAGE_JPEG, JpegEncoder::class],
+            [MediaType::IMAGE_BMP, BmpEncoder::class],
+            [MediaType::IMAGE_GIF, GifEncoder::class],
+            [MediaType::IMAGE_PNG, PngEncoder::class],
+            [MediaType::IMAGE_TIFF, TiffEncoder::class],
+            [MediaType::IMAGE_JP2, Jpeg2000Encoder::class],
+            [MediaType::IMAGE_HEIC, HeicEncoder::class],
+            [MediaType::IMAGE_HEIF, HeicEncoder::class],
         ];
     }
 
     public function testArgumentsNotSupportedByTargetEncoder(): void
     {
         $encoder = $this->testEncoder(
-            'png',
+            'image/png',
             [
                 'interlaced' => true, // is not ignored
                 'quality' => 10, // is ignored because png encoder has no quality argument

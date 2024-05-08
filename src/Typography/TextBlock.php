193324ec88bc5ad4039e57ce9b926ae28dfde813
Oliver Vogel <oliver@olivervogel.com>
2024-05-08 15:53:15 +0200

    Refactor line length calculation

--- src/Typography/TextBlock.php
+++ src/Typography/TextBlock.php
@@ -62,10 +62,10 @@ class TextBlock extends Collection
     {
         $lines = $this->lines();
         usort($lines, function (Line $a, Line $b) {
-            if (mb_strlen((string) $a) === mb_strlen((string) $b)) {
+            if ($a->length() === $b->length()) {
                 return 0;
             }
-            return mb_strlen((string) $a) > mb_strlen((string) $b) ? -1 : 1;
+            return $a->length() > $b->length() ? -1 : 1;
         });
 
         return $lines[0];

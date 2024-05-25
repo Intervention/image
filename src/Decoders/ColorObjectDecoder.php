27fd48170b56d56429c7c5c5df72194abc14cdb7
Oliver Vogel <oliver@olivervogel.com>
2024-05-25 10:34:15 +0200

    Move ColorObjectDecoder & ImageObjectDecoder
    Move ColorObjectDecoder & ImageObjectDecoder from drivers context
    because they can operate without any specific drivers information
    must not be specialized.

--- src/Decoders/ColorObjectDecoder.php
+++ src/Decoders/ColorObjectDecoder.php
@@ -4,8 +4,24 @@ declare(strict_types=1);
 
 namespace Intervention\Image\Decoders;
 
-use Intervention\Image\Drivers\SpecializableDecoder;
+use Intervention\Image\Drivers\AbstractDecoder;
+use Intervention\Image\Exceptions\DecoderException;
+use Intervention\Image\Interfaces\ColorInterface;
+use Intervention\Image\Interfaces\ImageInterface;
 
-class ColorObjectDecoder extends SpecializableDecoder
+class ColorObjectDecoder extends AbstractDecoder
 {
+    /**
+     * {@inheritdoc}
+     *
+     * @see DecoderInterface::decode()
+     */
+    public function decode(mixed $input): ImageInterface|ColorInterface
+    {
+        if (!is_a($input, ColorInterface::class)) {
+            throw new DecoderException('Unable to decode input');
+        }
+
+        return $input;
+    }
 }
--- src/Decoders/ImageObjectDecoder.php
+++ src/Decoders/ImageObjectDecoder.php
@@ -4,8 +4,24 @@ declare(strict_types=1);
 
 namespace Intervention\Image\Decoders;
 
-use Intervention\Image\Drivers\SpecializableDecoder;
+use Intervention\Image\Drivers\AbstractDecoder;
+use Intervention\Image\Exceptions\DecoderException;
+use Intervention\Image\Interfaces\ImageInterface;
+use Intervention\Image\Interfaces\ColorInterface;
 
-class ImageObjectDecoder extends SpecializableDecoder
+class ImageObjectDecoder extends AbstractDecoder
 {
+    /**
+     * {@inheritdoc}
+     *
+     * @see DecoderInterface::decode()
+     */
+    public function decode(mixed $input): ImageInterface|ColorInterface
+    {
+        if (!is_a($input, ImageInterface::class)) {
+            throw new DecoderException('Unable to decode input');
+        }
+
+        return $input;
+    }
 }

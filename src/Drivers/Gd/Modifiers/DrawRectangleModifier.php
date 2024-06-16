470b5bfdd8f4f500a1bcbee07c0f7d4a7d9d4fe7
Oliver Vogel <oliver@olivervogel.com>
2024-06-16 10:44:59 +0200

    Add imagesetthickness on GD's draw modifiers
    Make sure that no border is drawn for background colors

--- src/Drivers/Gd/Modifiers/DrawRectangleModifier.php
+++ src/Drivers/Gd/Modifiers/DrawRectangleModifier.php
@@ -22,6 +22,7 @@ class DrawRectangleModifier extends GenericDrawRectangleModifier implements Spec
             // draw background
             if ($this->drawable->hasBackgroundColor()) {
                 imagealphablending($frame->native(), true);
+                imagesetthickness($frame->native(), 0);
                 imagefilledrectangle(
                     $frame->native(),
                     $position->x(),

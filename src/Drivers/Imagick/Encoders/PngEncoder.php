cf40c11c8b71840ba833de1f0370a5a06916fdfd
Oliver Vogel <oliver@olivervogel.com>
2024-09-19 17:15:16 +0200

    Refactor code

--- src/Drivers/Imagick/Encoders/PngEncoder.php
+++ src/Drivers/Imagick/Encoders/PngEncoder.php
@@ -5,11 +5,7 @@ declare(strict_types=1);
 namespace Intervention\Image\Drivers\Imagick\Encoders;
 
 use Imagick;
-use ImagickException;
 use Intervention\Image\Encoders\PngEncoder as GenericPngEncoder;
-use Intervention\Image\Exceptions\AnimationException;
-use Intervention\Image\Exceptions\RuntimeException;
-use Intervention\Image\Exceptions\ColorException;
 use Intervention\Image\Interfaces\EncodedImageInterface;
 use Intervention\Image\Interfaces\ImageInterface;
 use Intervention\Image\Interfaces\SpecializedInterface;
@@ -23,7 +19,15 @@ class PngEncoder extends GenericPngEncoder implements SpecializedInterface
      */
     public function encode(ImageInterface $image): EncodedImageInterface
     {
-        $output = $this->prepareOutput($image);
+        if ($this->indexed) {
+            // reduce colors
+            $output = clone $image;
+            $output->reduceColors(256);
+
+            $output = $output->core()->native();
+        } else {
+            $output = clone $image->core()->native();
+        }
 
         $output->setCompression(Imagick::COMPRESSION_ZIP);
         $output->setImageCompression(Imagick::COMPRESSION_ZIP);
@@ -33,42 +37,10 @@ class PngEncoder extends GenericPngEncoder implements SpecializedInterface
         }
 
         return $this->createEncodedImage(function ($pointer) use ($output) {
-            $output->writeImageFile($pointer, $this->format());
+            $output->writeImageFile(
+                $pointer,
+                $this->indexed ? 'PNG' : 'PNG32',
+            );
         });
-    }
-
-    /**
-     * Prepare given image instance for PNG format output according to encoder settings
-     *
-     * @param ImageInterface $image
-     * @throws AnimationException
-     * @throws RuntimeException
-     * @throws ColorException
-     * @throws ImagickException
-     * @return Imagick
-     */
-    private function prepareOutput(ImageInterface $image): Imagick
-    {
-        $output = clone $image;
-
-        if ($this->indexed) {
-            // reduce colors
-            $output->reduceColors(256);
-            $output = $output->core()->native();
-
-            return $output;
-        }
-
-        $output = clone $image->core()->native();
-
-        return $output;
-    }
-
-    private function format(): string
-    {
-        return match ($this->indexed) {
-            true => 'PNG',
-            false => 'PNG32', // ensure to encode PNG image type 6 (true color alpha)
-        };
     }
 }

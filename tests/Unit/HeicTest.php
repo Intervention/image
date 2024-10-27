639ba48a07235f09efb8458fa7ca87617b048fa4
Oliver Vogel <oliver@olivervogel.com>
2024-10-27 19:49:39 +0100

    Adapt

--- tests/Unit/HeicTest.php
+++ tests/Unit/HeicTest.php
@@ -5,20 +5,34 @@ declare(strict_types=1);
 namespace Intervention\Image\Tests\Unit;
 
 use Intervention\Image\Tests\BaseTestCase;
-use Intervention\Image\Encoders\MediaTypeEncoder;
 use Imagick;
-use Intervention\Image\ImageManager;
+use ImagickPixel;
 
 final class HeicTest extends BaseTestCase
 {
     public function testEncoding(): void
     {
-        $manager = ImageManager::imagick();
-        $input = $manager->create(100, 100)->encode(new MediaTypeEncoder('image/heic'));
-
-
+        // create
+        $background = new ImagickPixel('rgba(255, 255, 255, 0)');
         $imagick = new Imagick();
-        $imagick->readImageBlob($input->toString());
+        $imagick->newImage(100, 100, $background, 'png');
+        $imagick->setType(Imagick::IMGTYPE_UNDEFINED);
+        $imagick->setImageType(Imagick::IMGTYPE_UNDEFINED);
+        $imagick->setColorspace(Imagick::COLORSPACE_SRGB);
+        $imagick->setImageResolution(96, 96);
+        $imagick->setImageBackgroundColor($background);
+
+        // encode
+        $imagick->setFormat('HEIC');
+        $imagick->setImageFormat('HEIC');
+        $imagick->setCompressionQuality(75);
+        $imagick->setImageCompressionQuality(75);
+        $encoded = $imagick->getImagesBlob();
+
+        // re-read
+        $imagick = new Imagick();
+        $imagick->readImageBlob($encoded);
+
         $this->assertInstanceOf(Imagick::class, $imagick);
     }
 }

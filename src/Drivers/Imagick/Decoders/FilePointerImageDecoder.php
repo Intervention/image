d5718568d862f2fc1c7f0453246fff205a04267d
Oliver Vogel <oliver@olivervogel.com>
2024-10-27 19:36:14 +0100

    Adapt

--- src/Drivers/Imagick/Decoders/FilePointerImageDecoder.php
+++ src/Drivers/Imagick/Decoders/FilePointerImageDecoder.php
@@ -18,7 +18,7 @@ class FilePointerImageDecoder extends BinaryImageDecoder
     public function decode(mixed $input): ImageInterface|ColorInterface
     {
         if (!is_resource($input) || !in_array(get_resource_type($input), ['file', 'stream'])) {
-            throw new DecoderException('Unable to decode input');
+            throw new DecoderException('Input is no file pointer resource.');
         }
 
         $contents = '';

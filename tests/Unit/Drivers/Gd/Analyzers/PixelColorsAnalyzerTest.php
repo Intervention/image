8e12c2e15265eee68758236d20443da5702d8cf2
Oliver Vogel <oliver@olivervogel.com>
2024-12-28 11:48:09 +0100

    Add tests

--- tests/Unit/Drivers/Gd/Analyzers/PixelColorsAnalyzerTest.php
+++ tests/Unit/Drivers/Gd/Analyzers/PixelColorsAnalyzerTest.php
@@ -16,7 +16,18 @@ use Intervention\Image\Tests\GdTestCase;
 #[CoversClass(PixelColorsAnalyzer::class)]
 final class PixelColorsAnalyzerTest extends GdTestCase
 {
-    public function testAnalyze(): void
+    public function testAnalyzeAnimated(): void
+    {
+        $image = $this->readTestImage('animation.gif');
+        $analyzer = new PixelColorsAnalyzer(0, 0);
+        $analyzer->setDriver(new Driver());
+        $result = $analyzer->analyze($image);
+        $this->assertInstanceOf(Collection::class, $result);
+        $colors = array_map(fn(ColorInterface $color) => $color->toHex(), $result->toArray());
+        $this->assertEquals($colors, ["394b63", "394b63", "394b63", "ffa601", "ffa601", "ffa601", "ffa601", "394b63"]);
+    }
+
+    public function testAnalyzeNonAnimated(): void
     {
         $image = $this->readTestImage('tile.png');
         $analyzer = new PixelColorsAnalyzer(0, 0);

955563a9a0ee50846989d58160fb889ccb4d74cb
Oliver Vogel <oliver@olivervogel.com>
2025-01-10 17:26:06 +0100

    Add doc block

--- tests/ImagickTestCase.php
+++ tests/ImagickTestCase.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Intervention\Image\Tests;
 
 use Imagick;
+use ImagickException;
 use ImagickPixel;
 use Intervention\Image\Decoders\FilePathImageDecoder;
 use Intervention\Image\Drivers\Imagick\Core;
@@ -20,6 +21,14 @@ abstract class ImagickTestCase extends BaseTestCase
         );
     }
 
+    /**
+     * Create test image with red (#ff0000) background
+     *
+     * @param int $width
+     * @param int $height
+     * @return Image
+     * @throws ImagickException
+     */
     public static function createTestImage(int $width, int $height): Image
     {
         $background = new ImagickPixel('rgb(255, 0, 0)');

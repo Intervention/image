9032a5a8483c905d25b82adeb075eda3b80d3a71
Oliver Vogel <oliver@olivervogel.com>
2025-05-18 09:39:08 +0200

    Enable wrapping of non-western text in TextModifier::class

--- src/Typography/Line.php
+++ src/Typography/Line.php
@@ -36,7 +36,7 @@ class Line implements IteratorAggregate, Countable, Stringable
         protected PointInterface $position = new Point()
     ) {
         if (is_string($text)) {
-            $this->segments = explode(" ", $text);
+            $this->segments = $this->splitToSegments($text);
         }
     }
 
@@ -88,7 +88,7 @@ class Line implements IteratorAggregate, Countable, Stringable
 
     /**
      * Count segments (individual words including punctuation marks) of line
-    *
+     *
      * @return int
      */
     public function count(): int
@@ -107,12 +107,53 @@ class Line implements IteratorAggregate, Countable, Stringable
     }
 
     /**
+     * Dermine if words are sperarated by spaces in the written language of the given text
+     */
+    private function wordsSeperatedBySpaces(string $text): bool
+    {
+        return 1 !== preg_match(
+            '/[' .
+            '\x{4E00}-\x{9FFF}' . // chinese
+            '\x{3040}-\x{309F}' . // hiragana (japanese)
+            '\x{30A0}-\x{30FF}' . // katakana (japanese)
+            '\x{0E00}-\x{0E7F}' . // thai
+            ']/u',
+            $text
+        );
+    }
+
+    /**
+     * Split text to segments depending on the written language of the given text
+     */
+    private function splitToSegments(string $text): array
+    {
+        if ($this->wordsSeperatedBySpaces($text)) {
+            return explode(" ", $text);
+        }
+
+        $segments = [];
+        $text = trim($text);
+        $length = mb_strlen($text, 'UTF-8');
+        for ($i = 0; $i < $length; $i++) {
+            $segments[] = mb_substr($text, $i, 1, 'UTF-8');
+        }
+
+        return $segments;
+    }
+
+    /**
      * Cast line to string
      *
      * @return string
      */
     public function __toString(): string
     {
-        return implode(" ", $this->segments);
+        $string = implode("", $this->segments);
+
+        if ($this->wordsSeperatedBySpaces($string)) {
+            return implode(" ", $this->segments);
+        }
+
+        return $string;
     }
 }

d0f097b8a3fa8fb758efc9440b513aa3833cda17
Oliver Vogel <oliver@olivervogel.com>
2025-05-22 19:26:23 +0200

    Enable wrapping for non western text (#1444)

--- src/Typography/Line.php
+++ src/Typography/Line.php
@@ -36,7 +36,7 @@ class Line implements IteratorAggregate, Countable, Stringable
         protected PointInterface $position = new Point()
     ) {
         if (is_string($text)) {
-            $this->segments = explode(" ", $text);
+            $this->segments = $this->wordsSeperatedBySpaces($text) ? explode(" ", $text) : mb_str_split($text);
         }
     }
 
@@ -88,7 +88,7 @@ class Line implements IteratorAggregate, Countable, Stringable
 
     /**
      * Count segments (individual words including punctuation marks) of line
-    *
+     *
      * @return int
      */
     public function count(): int
@@ -107,12 +107,35 @@ class Line implements IteratorAggregate, Countable, Stringable
     }
 
     /**
+     * Dermine if words are sperarated by spaces in the written language of the given text
+     */
+    private function wordsSeperatedBySpaces(string $text): bool
+    {
+        return 1 !== preg_match(
+            '/[' .
+            '\x{4E00}-\x{9FFF}' . // CJK Unified Ideographs (chinese)
+            '\x{3400}-\x{4DBF}' . // CJK Unified Ideographs Extension A (chinese)
+            '\x{3040}-\x{309F}' . // hiragana (japanese)
+            '\x{30A0}-\x{30FF}' . // katakana (japanese)
+            '\x{0E00}-\x{0E7F}' . // thai
+            ']/u',
+            $text
+        );
+    }
+
+    /**
      * Cast line to string
      *
      * @return string
      */
     public function __toString(): string
     {
-        return implode(" ", $this->segments);
+        $string = implode("", $this->segments);
+
+        if ($this->wordsSeperatedBySpaces($string)) {
+            return implode(" ", $this->segments);
+        }
+
+        return $string;
     }
 }

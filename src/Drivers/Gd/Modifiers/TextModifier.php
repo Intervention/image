8b8c41b976292a0b690a812b21c539efceb04eef
Oliver Vogel <oliver@olivervogel.com>
2025-12-17 14:38:29 +0100

    Fix issue with GD and relative font paths
    This patch builds full path to a font file with GD driver to make sure
    to pass absolute path to imagettftext(). This is because of issues with
    different GD versions behaving differently when passing relative paths
    to imagettftext()

--- src/Drivers/Gd/Modifiers/TextModifier.php
+++ src/Drivers/Gd/Modifiers/TextModifier.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Intervention\Image\Drivers\Gd\Modifiers;
 
 use Intervention\Image\Exceptions\ColorException;
+use Intervention\Image\Exceptions\FontException;
 use Intervention\Image\Exceptions\RuntimeException;
 use Intervention\Image\Interfaces\ImageInterface;
 use Intervention\Image\Interfaces\SpecializedInterface;
@@ -26,32 +27,40 @@ class TextModifier extends GenericTextModifier implements SpecializedInterface
         $textColor = $this->gdTextColor($image);
         $strokeColor = $this->gdStrokeColor($image);
 
+        // build full path to font file to make sure to pass absolute path to imageftbbox()
+        // because of issues with different GD version behaving differently when passing
+        // relative paths to imagettftext()
+        $fontPath = $this->font->hasFilename() ? realpath($this->font->filename()) : false;
+        if ($this->font->hasFilename() && $fontPath === false) {
+            throw new FontException('Font file ' . $this->font->filename() . ' does not exist.');
+        }
+
         foreach ($image as $frame) {
             imagealphablending($frame->native(), true);
             if ($this->font->hasFilename()) {
                 foreach ($lines as $line) {
                     foreach ($this->strokeOffsets($this->font) as $offset) {
                         imagettftext(
-                            $frame->native(),
-                            $fontProcessor->nativeFontSize($this->font),
-                            $this->font->angle() * -1,
-                            $line->position()->x() + $offset->x(),
-                            $line->position()->y() + $offset->y(),
-                            $strokeColor,
-                            $this->font->filename(),
-                            (string) $line
+                            image: $frame->native(),
+                            size: $fontProcessor->nativeFontSize($this->font),
+                            angle: $this->font->angle() * -1,
+                            x: $line->position()->x() + $offset->x(),
+                            y: $line->position()->y() + $offset->y(),
+                            color: $strokeColor,
+                            font_filename: $fontPath,
+                            text: (string) $line
                         );
                     }
 
                     imagettftext(
-                        $frame->native(),
-                        $fontProcessor->nativeFontSize($this->font),
-                        $this->font->angle() * -1,
-                        $line->position()->x(),
-                        $line->position()->y(),
-                        $textColor,
-                        $this->font->filename(),
-                        (string) $line
+                        image: $frame->native(),
+                        size: $fontProcessor->nativeFontSize($this->font),
+                        angle: $this->font->angle() * -1,
+                        x: $line->position()->x(),
+                        y: $line->position()->y(),
+                        color: $textColor,
+                        font_filename: $fontPath,
+                        text: (string) $line
                     );
                 }
             } else {

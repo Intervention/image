5f4803dcbb6bfde72c3dc43236125617ce031a71
Oliver Vogel <oliver@olivervogel.com>
2026-01-04 11:54:18 +0100

    Add todos

--- src/Interfaces/EncodedImageInterface.php
+++ src/Interfaces/EncodedImageInterface.php
@@ -9,15 +9,15 @@ interface EncodedImageInterface extends FileInterface
     /**
      * Return Media (MIME) Type of encoded image.
      */
-    public function mediaType(): string;
+    public function mediaType(): string; // todo: maybe move to FileInterface
 
     /**
      * Alias of self::mediaType().
      */
-    public function mimetype(): string;
+    public function mimetype(): string; // todo: maybe move to FileInterface
 
     /**
      * Transform encoded image data into an data uri string.
      */
-    public function toDataUri(): DataUriInterface;
+    public function toDataUri(): DataUriInterface; // todo: maybe move to FileInterface
 }

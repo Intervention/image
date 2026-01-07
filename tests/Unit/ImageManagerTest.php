8d18b40578248ed6127ee2b652d766e1a5d555c9
Oliver Vogel <oliver@olivervogel.com>
2026-01-07 16:00:43 +0100

    Combine test classes

--- tests/Unit/ImageManagerTest.php
+++ tests/Unit/ImageManagerTest.php
@@ -4,62 +4,76 @@ declare(strict_types=1);
 
 namespace Intervention\Image\Tests\Unit;
 
+use Intervention\Image\Tests\BaseTestCase;
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\Attributes\RequiresPhpExtension;
 use Intervention\Image\Decoders\BinaryImageDecoder;
 use Intervention\Image\Decoders\FilePathImageDecoder;
-use Intervention\Image\Drivers\Imagick\Driver;
+use Intervention\Image\Drivers\Gd\Driver as GdDriver;
+use Intervention\Image\Drivers\Imagick\Driver as ImagickDriver;
 use Intervention\Image\ImageManager;
+use Intervention\Image\Interfaces\DriverInterface;
 use Intervention\Image\Interfaces\FrameInterface;
 use Intervention\Image\Interfaces\ImageInterface;
-use Intervention\Image\Tests\BaseTestCase;
+use Intervention\Image\Tests\Providers\DriverProvider;
 use Intervention\Image\Tests\Resource;
+use PHPUnit\Framework\Attributes\DataProviderExternal;
 
 #[CoversClass(ImageManager::class)]
+#[RequiresPhpExtension('gd')]
 #[RequiresPhpExtension('imagick')]
-final class ImageManagerTestImagick extends BaseTestCase
+class ImageManagerTest extends BaseTestCase
 {
-    public function testConstructor(): void
+    #[DataProviderExternal(DriverProvider::class, 'drivers')]
+    public function testConstructor(DriverInterface $driver): void
     {
-        $manager = new ImageManager(new Driver());
+        $manager = new ImageManager($driver);
         $this->assertInstanceOf(ImageManager::class, $manager);
 
-        $manager = new ImageManager(Driver::class);
+        $manager = new ImageManager($driver::class);
         $this->assertInstanceOf(ImageManager::class, $manager);
     }
 
-    public function testWithDriver(): void
+    #[DataProviderExternal(DriverProvider::class, 'drivers')]
+    public function testWithDriver(DriverInterface $driver): void
     {
-        $manager = ImageManager::withDriver(new Driver());
+        $manager = ImageManager::withDriver($driver);
         $this->assertInstanceOf(ImageManager::class, $manager);
 
-        $manager = ImageManager::withDriver(Driver::class);
+        $manager = ImageManager::withDriver($driver::class);
         $this->assertInstanceOf(ImageManager::class, $manager);
     }
 
-    public function testDriver(): void
+    #[DataProviderExternal(DriverProvider::class, 'drivers')]
+    public function testDriver(DriverInterface $driver): void
     {
-        $driver = new Driver();
         $manager = ImageManager::withDriver($driver);
         $this->assertEquals($driver, $manager->driver());
     }
 
     public function testDriverStatic(): void
     {
+        $manager = ImageManager::gd();
+        $this->assertInstanceOf(ImageManager::class, $manager);
+        $this->assertInstanceOf(GdDriver::class, $manager->driver());
+
         $manager = ImageManager::imagick();
         $this->assertInstanceOf(ImageManager::class, $manager);
+        $this->assertInstanceOf(ImagickDriver::class, $manager->driver());
     }
 
-    public function testCreate(): void
+    #[DataProviderExternal(DriverProvider::class, 'drivers')]
+    public function testCreate(DriverInterface $driver): void
     {
-        $manager = new ImageManager(Driver::class);
+        $manager = new ImageManager($driver);
         $image = $manager->createImage(5, 4);
         $this->assertInstanceOf(ImageInterface::class, $image);
     }
 
-    public function testCreateAnimated(): void
+    #[DataProviderExternal(DriverProvider::class, 'drivers')]
+    public function testCreateAnimated(DriverInterface $driver): void
     {
-        $manager = new ImageManager(Driver::class);
+        $manager = new ImageManager($driver);
         $image = $manager->createImage(12, 8, function ($animation): void {
             $animation->add(Resource::create('red.gif')->path(), .25);
             $animation->add(Resource::create('green.gif')->path(), .25);
@@ -77,37 +91,42 @@ final class ImageManagerTestImagick extends BaseTestCase
         ));
     }
 
-    public function testDecodeUsingDecoderClassname(): void
+    #[DataProviderExternal(DriverProvider::class, 'drivers')]
+    public function testDecodeUsingDecoderClassname(DriverInterface $driver): void
     {
-        $manager = new ImageManager(Driver::class);
+        $manager = new ImageManager($driver);
         $image = $manager->decode(Resource::create('red.gif')->path(), FilePathImageDecoder::class);
         $this->assertInstanceOf(ImageInterface::class, $image);
     }
 
-    public function testDecodeUsingDecoderInstance(): void
+    #[DataProviderExternal(DriverProvider::class, 'drivers')]
+    public function testDecodeUsingDecoderInstance(DriverInterface $driver): void
     {
-        $manager = new ImageManager(Driver::class);
+        $manager = new ImageManager($driver);
         $image = $manager->decode(Resource::create('red.gif')->path(), new FilePathImageDecoder());
         $this->assertInstanceOf(ImageInterface::class, $image);
     }
 
-    public function testDecodeUsingDecoderClassnameArray(): void
+    #[DataProviderExternal(DriverProvider::class, 'drivers')]
+    public function testDecodeUsingDecoderClassnameArray(DriverInterface $driver): void
     {
-        $manager = new ImageManager(Driver::class);
+        $manager = new ImageManager($driver);
         $image = $manager->decode(Resource::create('red.gif')->path(), [FilePathImageDecoder::class]);
         $this->assertInstanceOf(ImageInterface::class, $image);
     }
 
-    public function testDecodeUsingDecoderInstanceArray(): void
+    #[DataProviderExternal(DriverProvider::class, 'drivers')]
+    public function testDecodeUsingDecoderInstanceArray(DriverInterface $driver): void
     {
-        $manager = new ImageManager(Driver::class);
+        $manager = new ImageManager($driver);
         $image = $manager->decode(Resource::create('red.gif')->path(), [new FilePathImageDecoder()]);
         $this->assertInstanceOf(ImageInterface::class, $image);
     }
 
-    public function testDecodeUsingDecoderInstanceArrayMultiple(): void
+    #[DataProviderExternal(DriverProvider::class, 'drivers')]
+    public function testDecodeUsingDecoderInstanceArrayMultiple(DriverInterface $driver): void
     {
-        $manager = new ImageManager(Driver::class);
+        $manager = new ImageManager($driver);
         $image = $manager->decode(Resource::create('red.gif')->path(), [
             new BinaryImageDecoder(),
             new FilePathImageDecoder(),
@@ -115,46 +134,52 @@ final class ImageManagerTestImagick extends BaseTestCase
         $this->assertInstanceOf(ImageInterface::class, $image);
     }
 
-    public function testDecodeRotationAdjustment(): void
+    #[DataProviderExternal(DriverProvider::class, 'drivers')]
+    public function testDecodeRotationAdjustment(DriverInterface $driver): void
     {
-        $manager = new ImageManager(Driver::class);
+        $manager = new ImageManager($driver);
         $image = $manager->decodeFrom(path: Resource::create('orientation.jpg')->path());
         $this->assertColor(1, 0, 254, 1, $image->colorAt(3, 3));
     }
 
-    public function testDecodeWithoutRotationAdjustment(): void
+    #[DataProviderExternal(DriverProvider::class, 'drivers')]
+    public function testDecodeWithoutRotationAdjustment(DriverInterface $driver): void
     {
-        $manager = new ImageManager(Driver::class, autoOrientation: false);
+        $manager = new ImageManager($driver, autoOrientation: false);
         $image = $manager->decodeFrom(path: Resource::create('orientation.jpg')->path());
         $this->assertColor(250, 2, 3, 1, $image->colorAt(3, 3));
     }
 
-    public function testDecodeAnimation(): void
+    #[DataProviderExternal(DriverProvider::class, 'drivers')]
+    public function testDecodeAnimation(DriverInterface $driver): void
     {
-        $manager = new ImageManager(Driver::class);
+        $manager = new ImageManager($driver);
         $image = $manager->decodeFrom(path: Resource::create('animation.gif')->path());
         $this->assertTrue($image->isAnimated());
     }
 
-    public function testDecodeAnimationDiscarded(): void
+    #[DataProviderExternal(DriverProvider::class, 'drivers')]
+    public function testDecodeAnimationDiscarded(DriverInterface $driver): void
     {
-        $manager = new ImageManager(Driver::class, decodeAnimation: false);
+        $manager = new ImageManager($driver, decodeAnimation: false);
         $image = $manager->decodeFrom(path: Resource::create('animation.gif')->path());
         $this->assertFalse($image->isAnimated());
     }
 
-    public function testApplyBackgroundColor(): void
+    #[DataProviderExternal(DriverProvider::class, 'drivers')]
+    public function testApplyBackgroundColorDefault(DriverInterface $driver): void
     {
-        $manager = new ImageManager(Driver::class);
+        $manager = new ImageManager($driver);
         $image = $manager->decodeFrom(path: Resource::create('blocks.png')->path());
         $result = $image->background();
         $this->assertColor(255, 255, 255, 1, $image->colorAt(530, 0));
         $this->assertColor(255, 255, 255, 1, $result->colorAt(530, 0));
     }
 
-    public function testApplyBackgroundColorConfigured(): void
+    #[DataProviderExternal(DriverProvider::class, 'drivers')]
+    public function testApplyBackgroundColorConfigured(DriverInterface $driver): void
     {
-        $manager = new ImageManager(Driver::class, backgroundColor: 'ff5500');
+        $manager = new ImageManager($driver, backgroundColor: 'ff5500');
         $image = $manager->decodeFrom(path: Resource::create('blocks.png')->path());
         $result = $image->background();
         $this->assertColor(255, 85, 0, 1, $image->colorAt(530, 0));

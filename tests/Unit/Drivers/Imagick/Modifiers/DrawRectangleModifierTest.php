aa54aff54621a02d0025d009c190a6efb159cee8
Oliver Vogel <oliver@olivervogel.com>
2026-02-28 09:45:59 +0100

    Add tests

--- tests/Unit/Drivers/Imagick/Modifiers/DrawRectangleModifierTest.php
+++ tests/Unit/Drivers/Imagick/Modifiers/DrawRectangleModifierTest.php
@@ -6,6 +6,7 @@ namespace Intervention\Image\Tests\Unit\Drivers\Imagick\Modifiers;
 
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\Attributes\RequiresPhpExtension;
+use Intervention\Image\Modifiers\AbstractDrawModifier;
 use Intervention\Image\Modifiers\DrawRectangleModifier;
 use Intervention\Image\Geometry\Point;
 use Intervention\Image\Geometry\Rectangle;
@@ -14,6 +15,7 @@ use Intervention\Image\Tests\ImagickTestCase;
 #[RequiresPhpExtension('imagick')]
 #[CoversClass(\Intervention\Image\Modifiers\DrawRectangleModifier::class)]
 #[CoversClass(\Intervention\Image\Drivers\Imagick\Modifiers\DrawRectangleModifier::class)]
+#[CoversClass(AbstractDrawModifier::class)]
 final class DrawRectangleModifierTest extends ImagickTestCase
 {
     public function testApply(): void
@@ -24,5 +26,15 @@ final class DrawRectangleModifierTest extends ImagickTestCase
         $rectangle->setBackgroundColor('ffffff');
         $image->modify(new DrawRectangleModifier($rectangle));
         $this->assertEquals('ffffff', $image->colorAt(14, 14)->toHex());
+    }
+
+    public function testApplyWithBorder(): void
+    {
+        $image = $this->readTestImage('trim.png');
+        $rectangle = new Rectangle(10, 10, new Point(0, 0));
+        $rectangle->setBackgroundColor('ffffff');
+        $rectangle->setBorder('ff0000', 1);
+        $image->modify(new DrawRectangleModifier($rectangle));
+        $this->assertEquals('ff0000', $image->colorAt(0, 0)->toHex());
     }
 }
--- tests/Unit/Drivers/SpecializableDecoderTest.php
+++ tests/Unit/Drivers/SpecializableDecoderTest.php
@@ -7,7 +7,6 @@ namespace Intervention\Image\Tests\Unit\Drivers;
 use Intervention\Image\Drivers\SpecializableDecoder;
 use Intervention\Image\Exceptions\DriverException;
 use Intervention\Image\Tests\BaseTestCase;
-use Mockery;
 use PHPUnit\Framework\Attributes\CoversClass;
 
 #[CoversClass(SpecializableDecoder::class)]
@@ -15,8 +14,19 @@ final class SpecializableDecoderTest extends BaseTestCase
 {
     public function testDecode(): void
     {
-        $decoder = Mockery::mock(SpecializableDecoder::class)->makePartial();
+        $decoder = new class () extends SpecializableDecoder {
+            //
+        };
         $this->expectException(DriverException::class);
         $decoder->decode(null);
+    }
+
+    public function testSupports(): void
+    {
+        $decoder = new class () extends SpecializableDecoder {
+            //
+        };
+        $this->expectException(DriverException::class);
+        $decoder->supports(null);
     }
 }

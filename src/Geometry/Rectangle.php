5c0cd3c513d169fd88da84d0a6185364ad8f8894
Oliver Vogel <oliver@olivervogel.com>
2026-05-14 07:34:24 +0200

    Ensure compatibility

--- src/Geometry/Rectangle.php
+++ src/Geometry/Rectangle.php
@@ -4,14 +4,21 @@ declare(strict_types=1);
 
 namespace Intervention\Image\Geometry;
 
+use ArrayIterator;
+use DivisionByZeroError;
+use Intervention\Image\Alignment;
 use Intervention\Image\Exceptions\InvalidArgumentException;
 use Intervention\Image\Exceptions\RuntimeException;
+use Intervention\Image\Exceptions\StateException;
 use Intervention\Image\Geometry\Factories\RectangleFactory;
+use Intervention\Image\Geometry\Tools\Resizer;
 use Intervention\Image\Interfaces\DrawableFactoryInterface;
 use Intervention\Image\Interfaces\DrawableInterface;
 use Intervention\Image\Interfaces\PointInterface;
+use Intervention\Image\Interfaces\SizeInterface;
+use Traversable;
 
-class Rectangle extends Polygon implements DrawableInterface
+class Rectangle extends Polygon implements DrawableInterface, SizeInterface
 {
     /**
      * Create new rectangle.
@@ -133,5 +140,342 @@ class Rectangle extends Polygon implements DrawableInterface
         $adjustments($factory);
 
         return $factory->drawable();
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @deprecated Use Intervention\Image\Size::class instead.
+     * @see SizeInterface::aspectRatio()
+     *
+     * @throws RuntimeException
+     */
+    public function aspectRatio(): float
+    {
+        try {
+            return $this->width() / $this->height();
+        } catch (DivisionByZeroError) {
+            throw new RuntimeException('Division by zero');
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @deprecated Use Intervention\Image\Size::class instead.
+     * @see SizeInterface::fitsWithin()
+     */
+    public function fitsWithin(SizeInterface $size): bool
+    {
+        if ($this->width() > $size->width()) {
+            return false;
+        }
+
+        if ($this->height() > $size->height()) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @deprecated Use Intervention\Image\Size::class instead.
+     * @see SizeInterface::isLandscape()
+     */
+    public function isLandscape(): bool
+    {
+        return $this->width() > $this->height();
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @deprecated Use Intervention\Image\Size::class instead.
+     * @see SizeInterface::isPortrait()
+     */
+    public function isPortrait(): bool
+    {
+        return $this->width() < $this->height();
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @deprecated Use Intervention\Image\Size::class instead.
+     * @see SizeInterface::setPivot()
+     */
+    public function setPivot(PointInterface $pivot): self
+    {
+        $this->pivot = $pivot;
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @deprecated Use Intervention\Image\Size::class instead.
+     * @see SizeInterface::movePivot()
+     *
+     * @throws InvalidArgumentException
+     */
+    public function movePivot(string|Alignment $alignment, int $x = 0, int $y = 0): self
+    {
+        $alignment = Alignment::create($alignment); // normalize alignment
+
+        $point = match ($alignment) {
+            Alignment::TOP => new Point(
+                intval(round($this->width() / 2)) + $x,
+                $y,
+            ),
+            Alignment::TOP_RIGHT => new Point(
+                $this->width() - $x,
+                $y,
+            ),
+            Alignment::LEFT => new Point(
+                $x,
+                intval(round($this->height() / 2)) + $y,
+            ),
+            Alignment::RIGHT => new Point(
+                $this->width() - $x,
+                intval(round($this->height() / 2)) + $y,
+            ),
+            Alignment::BOTTOM_LEFT => new Point(
+                $x,
+                $this->height() - $y,
+            ),
+            Alignment::BOTTOM => new Point(
+                intval(round($this->width() / 2)) + $x,
+                $this->height() - $y,
+            ),
+            Alignment::BOTTOM_RIGHT => new Point(
+                $this->width() - $x,
+                $this->height() - $y,
+            ),
+            Alignment::CENTER => new Point(
+                intval(round($this->width() / 2)) + $x,
+                intval(round($this->height() / 2)) + $y,
+            ),
+            Alignment::TOP_LEFT => new Point(
+                $x,
+                $y,
+            ),
+        };
+
+        $this->pivot->setPosition(...$point);
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @deprecated Use Intervention\Image\Size::class instead.
+     * @see SizeInterface::alignPivotTo()
+     *
+     * @throws InvalidArgumentException
+     */
+    public function alignPivotTo(SizeInterface $size, string|Alignment $alignment): self
+    {
+        $reference = new self($size->width(), $size->height());
+        $reference->movePivot($alignment);
+
+        $this->movePivot($alignment)->setPivot(
+            $reference->offsetTo($this)
+        );
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @deprecated Use Intervention\Image\Size::class instead.
+     * @see SizeInterface::offsetTo()
+     */
+    public function offsetTo(SizeInterface $rectangle): PointInterface
+    {
+        return new Point(
+            $this->pivot()->x() - $rectangle->pivot()->x(),
+            $this->pivot()->y() - $rectangle->pivot()->y()
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @deprecated Use Intervention\Image\Size::class instead.
+     * @see SizeInterface::resize()
+     *
+     * @throws InvalidArgumentException
+     */
+    public function resize(?int $width = null, ?int $height = null): SizeInterface
+    {
+        try {
+            return $this->resizer($width, $height)->resize($this);
+        } catch (InvalidArgumentException $e) {
+            throw new InvalidArgumentException(
+                'Invalid target size ' . $width . 'x' . $height,
+                previous: $e,
+            );
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @deprecated Use Intervention\Image\Size::class instead.
+     * @see SizeInterface::resizeDown()
+     *
+     * @throws InvalidArgumentException
+     */
+    public function resizeDown(?int $width = null, ?int $height = null): SizeInterface
+    {
+        try {
+            return $this->resizer($width, $height)->resizeDown($this);
+        } catch (InvalidArgumentException $e) {
+            throw new InvalidArgumentException(
+                'Invalid target size ' . $width . 'x' . $height,
+                previous: $e,
+            );
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @deprecated Use Intervention\Image\Size::class instead.
+     * @see SizeInterface::scale()
+     *
+     * @throws InvalidArgumentException
+     */
+    public function scale(?int $width = null, ?int $height = null): SizeInterface
+    {
+        try {
+            return $this->resizer($width, $height)->scale($this);
+        } catch (InvalidArgumentException $e) {
+            throw new InvalidArgumentException(
+                'Invalid target size ' . $width . 'x' . $height,
+                previous: $e,
+            );
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @deprecated Use Intervention\Image\Size::class instead.
+     * @see SizeInterface::scaleDown()
+     *
+     * @throws InvalidArgumentException
+     */
+    public function scaleDown(?int $width = null, ?int $height = null): SizeInterface
+    {
+        try {
+            return $this->resizer($width, $height)->scaleDown($this);
+        } catch (InvalidArgumentException $e) {
+            throw new InvalidArgumentException(
+                'Invalid target size ' . $width . 'x' . $height,
+                previous: $e,
+            );
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @deprecated Use Intervention\Image\Size::class instead.
+     * @see SizeInterface::cover()
+     *
+     * @throws InvalidArgumentException
+     */
+    public function cover(int $width, int $height): SizeInterface
+    {
+        try {
+            return $this->resizer($width, $height)->cover($this);
+        } catch (InvalidArgumentException | StateException $e) {
+            throw new InvalidArgumentException(
+                'Invalid target size ' . $width . 'x' . $height,
+                previous: $e,
+            );
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @deprecated Use Intervention\Image\Size::class instead.
+     * @see SizeInterface::contain()
+     *
+     * @throws InvalidArgumentException
+     */
+    public function contain(int $width, int $height): SizeInterface
+    {
+        try {
+            return $this->resizer($width, $height)->contain($this);
+        } catch (StateException $e) {
+            throw new InvalidArgumentException(
+                'Invalid target size ' . $width . 'x' . $height,
+                previous: $e,
+            );
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @deprecated Use Intervention\Image\Size::class instead.
+     * @see SizeInterface::containDown()
+     *
+     * @throws InvalidArgumentException
+     */
+    public function containDown(int $width, int $height): SizeInterface
+    {
+        try {
+            return $this->resizer($width, $height)->containDown($this);
+        } catch (StateException $e) {
+            throw new InvalidArgumentException(
+                'Invalid target size ' . $width . 'x' . $height,
+                previous: $e,
+            );
+        }
+    }
+
+    /**
+     * Create resizer instance with given target size.
+     *
+     * @deprecated Use Intervention\Image\Size::class instead.
+     *
+     * @throws InvalidArgumentException
+     */
+    protected function resizer(?int $width = null, ?int $height = null): Resizer
+    {
+        return new Resizer($width, $height);
+    }
+
+    /**
+     * Implement iteration.
+     *
+     * @return Traversable<mixed>
+     */
+    public function getIterator(): Traversable
+    {
+        return new ArrayIterator([$this->width(), $this->height()]);
+    }
+
+    /**
+     * Show debug info for the current rectangle.
+     *
+     * @return array<string, int|object>
+     */
+    public function __debugInfo(): array
+    {
+        return [
+            'width' => $this->width(),
+            'height' => $this->height(),
+            'pivot' => $this->pivot,
+        ];
     }
 }
--- tests/Unit/Geometry/RectangleTest.php
+++ tests/Unit/Geometry/RectangleTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Intervention\Image\Tests\Unit\Geometry;
 
+use Intervention\Image\Alignment;
 use Intervention\Image\Geometry\Factories\RectangleFactory;
 use Intervention\Image\Geometry\Point;
 use Intervention\Image\Geometry\Rectangle;
@@ -60,14 +61,265 @@ final class RectangleTest extends BaseTestCase
         $this->assertEquals(800, $rectangle->height());
     }
 
+    public function testGetAspectRatio(): void
+    {
+        $rectangle = new Rectangle(800, 600);
+        $this->assertEquals(1.333, round($rectangle->aspectRatio(), 3));
+
+        $rectangle = new Rectangle(100, 100);
+        $this->assertEquals(1, $rectangle->aspectRatio());
+
+        $rectangle = new Rectangle(1920, 1080);
+        $this->assertEquals(1.778, round($rectangle->aspectRatio(), 3));
+    }
+
+    public function testFitsInto(): void
+    {
+        $rectangle = new Rectangle(800, 600);
+        $fits = $rectangle->fitsWithin(new Rectangle(100, 100));
+        $this->assertFalse($fits);
+
+        $rectangle = new Rectangle(800, 600);
+        $fits = $rectangle->fitsWithin(new Rectangle(1000, 100));
+        $this->assertFalse($fits);
+
+        $rectangle = new Rectangle(800, 600);
+        $fits = $rectangle->fitsWithin(new Rectangle(100, 1000));
+        $this->assertFalse($fits);
+
+        $rectangle = new Rectangle(800, 600);
+        $fits = $rectangle->fitsWithin(new Rectangle(800, 600));
+        $this->assertTrue($fits);
+
+        $rectangle = new Rectangle(800, 600);
+        $fits = $rectangle->fitsWithin(new Rectangle(1000, 1000));
+        $this->assertTrue($fits);
+
+        $rectangle = new Rectangle(100, 100);
+        $fits = $rectangle->fitsWithin(new Rectangle(800, 600));
+        $this->assertTrue($fits);
+
+        $rectangle = new Rectangle(100, 100);
+        $fits = $rectangle->fitsWithin(new Rectangle(80, 60));
+        $this->assertFalse($fits);
+    }
+
+    public function testIsLandscape(): void
+    {
+        $rectangle = new Rectangle(100, 100);
+        $this->assertFalse($rectangle->isLandscape());
+
+        $rectangle = new Rectangle(100, 200);
+        $this->assertFalse($rectangle->isLandscape());
+
+        $rectangle = new Rectangle(300, 200);
+        $this->assertTrue($rectangle->isLandscape());
+    }
+
+    public function testIsPortrait(): void
+    {
+        $rectangle = new Rectangle(100, 100);
+        $this->assertFalse($rectangle->isPortrait());
+
+        $rectangle = new Rectangle(200, 100);
+        $this->assertFalse($rectangle->isPortrait());
+
+        $rectangle = new Rectangle(200, 300);
+        $this->assertTrue($rectangle->isPortrait());
+    }
+
     public function testSetGetPivot(): void
     {
-        $box = new Rectangle(800, 600);
-        $pivot = $box->pivot();
+        $rectangle = new Rectangle(800, 600);
+        $pivot = $rectangle->pivot();
         $this->assertInstanceOf(Point::class, $pivot);
         $this->assertEquals(0, $pivot->x());
-        $result = $box->setPivot(new Point(10, 0));
+        $result = $rectangle->setPivot(new Point(10, 0));
         $this->assertInstanceOf(Rectangle::class, $result);
-        $this->assertEquals(10, $box->pivot()->x());
+        $this->assertEquals(10, $rectangle->pivot()->x());
+    }
+
+    public function testAlignPivot(): void
+    {
+        $rectangle = new Rectangle(640, 480);
+        $this->assertEquals(0, $rectangle->pivot()->x());
+        $this->assertEquals(0, $rectangle->pivot()->y());
+
+        $rectangle->movePivot(Alignment::TOP_LEFT, 3, 3);
+        $this->assertEquals(3, $rectangle->pivot()->x());
+        $this->assertEquals(3, $rectangle->pivot()->y());
+
+        $rectangle->movePivot(Alignment::TOP, 3, 3);
+        $this->assertEquals(323, $rectangle->pivot()->x());
+        $this->assertEquals(3, $rectangle->pivot()->y());
+
+        $rectangle->movePivot(Alignment::TOP_RIGHT, 3, 3);
+        $this->assertEquals(637, $rectangle->pivot()->x());
+        $this->assertEquals(3, $rectangle->pivot()->y());
+
+        $rectangle->movePivot(Alignment::LEFT, 3, 3);
+        $this->assertEquals(3, $rectangle->pivot()->x());
+        $this->assertEquals(243, $rectangle->pivot()->y());
+
+        $rectangle->movePivot(Alignment::CENTER, 3, 3);
+        $this->assertEquals(323, $rectangle->pivot()->x());
+        $this->assertEquals(243, $rectangle->pivot()->y());
+
+        $rectangle->movePivot(Alignment::RIGHT, 3, 3);
+        $this->assertEquals(637, $rectangle->pivot()->x());
+        $this->assertEquals(243, $rectangle->pivot()->y());
+
+        $rectangle->movePivot(Alignment::BOTTOM_LEFT, 3, 3);
+        $this->assertEquals(3, $rectangle->pivot()->x());
+        $this->assertEquals(477, $rectangle->pivot()->y());
+
+        $rectangle->movePivot(Alignment::BOTTOM, 3, 3);
+        $this->assertEquals(323, $rectangle->pivot()->x());
+        $this->assertEquals(477, $rectangle->pivot()->y());
+
+        $result = $rectangle->movePivot(Alignment::BOTTOM_RIGHT, 3, 3);
+        $this->assertEquals(637, $rectangle->pivot()->x());
+        $this->assertEquals(477, $rectangle->pivot()->y());
+
+        $this->assertInstanceOf(Rectangle::class, $result);
+    }
+
+    public function testAlignPivotTo(): void
+    {
+        $container = new Rectangle(800, 600);
+        $rectangle = new Rectangle(200, 100);
+        $rectangle->alignPivotTo($container, Alignment::CENTER);
+        $this->assertEquals(300, $rectangle->pivot()->x());
+        $this->assertEquals(250, $rectangle->pivot()->y());
+
+        $container = new Rectangle(800, 600);
+        $rectangle = new Rectangle(100, 100);
+        $rectangle->alignPivotTo($container, Alignment::CENTER);
+        $this->assertEquals(350, $rectangle->pivot()->x());
+        $this->assertEquals(250, $rectangle->pivot()->y());
+
+        $container = new Rectangle(800, 600);
+        $rectangle = new Rectangle(800, 600);
+        $rectangle->alignPivotTo($container, Alignment::CENTER);
+        $this->assertEquals(0, $rectangle->pivot()->x());
+        $this->assertEquals(0, $rectangle->pivot()->y());
+
+        $container = new Rectangle(100, 100);
+        $rectangle = new Rectangle(800, 600);
+        $rectangle->alignPivotTo($container, Alignment::CENTER);
+        $this->assertEquals(-350, $rectangle->pivot()->x());
+        $this->assertEquals(-250, $rectangle->pivot()->y());
+
+        $container = new Rectangle(100, 100);
+        $rectangle = new Rectangle(800, 600);
+        $rectangle->alignPivotTo($container, Alignment::BOTTOM_RIGHT);
+        $this->assertEquals(-700, $rectangle->pivot()->x());
+        $this->assertEquals(-500, $rectangle->pivot()->y());
+    }
+
+    public function testOffsetTo(): void
+    {
+        $container = new Rectangle(800, 600);
+        $rectangle = new Rectangle(200, 100);
+        $container->movePivot(Alignment::TOP_LEFT);
+        $rectangle->movePivot(Alignment::TOP_LEFT);
+        $pos = $container->offsetTo($rectangle);
+        $this->assertEquals(0, $pos->x());
+        $this->assertEquals(0, $pos->y());
+
+        $container = new Rectangle(800, 600);
+        $rectangle = new Rectangle(200, 100);
+        $container->movePivot(Alignment::CENTER);
+        $rectangle->movePivot(Alignment::TOP_LEFT);
+        $pos = $container->offsetTo($rectangle);
+        $this->assertEquals(400, $pos->x());
+        $this->assertEquals(300, $pos->y());
+
+        $container = new Rectangle(800, 600);
+        $rectangle = new Rectangle(200, 100);
+        $container->movePivot(Alignment::BOTTOM_RIGHT);
+        $rectangle->movePivot(Alignment::TOP_RIGHT);
+        $pos = $container->offsetTo($rectangle);
+        $this->assertEquals(600, $pos->x());
+        $this->assertEquals(600, $pos->y());
+
+        $container = new Rectangle(800, 600);
+        $rectangle = new Rectangle(200, 100);
+        $container->movePivot(Alignment::CENTER);
+        $rectangle->movePivot(Alignment::CENTER);
+        $pos = $container->offsetTo($rectangle);
+        $this->assertEquals(300, $pos->x());
+        $this->assertEquals(250, $pos->y());
+
+        $container = new Rectangle(100, 200);
+        $rectangle = new Rectangle(100, 100);
+        $container->movePivot(Alignment::CENTER);
+        $rectangle->movePivot(Alignment::CENTER);
+        $pos = $container->offsetTo($rectangle);
+        $this->assertEquals(0, $pos->x());
+        $this->assertEquals(50, $pos->y());
+    }
+
+    public function testResize(): void
+    {
+        $rectangle = new Rectangle(800, 600);
+        $result = $rectangle->resize(300, 200);
+        $this->assertEquals(300, $result->width());
+        $this->assertEquals(200, $result->height());
+    }
+
+    public function testResizeDown(): void
+    {
+        $rectangle = new Rectangle(800, 600);
+        $result = $rectangle->resizeDown(3000, 200);
+        $this->assertEquals(800, $result->width());
+        $this->assertEquals(200, $result->height());
+    }
+
+    public function testScale(): void
+    {
+        $rectangle = new Rectangle(800, 600);
+        $result = $rectangle->scale(height: 1200);
+        $this->assertEquals(800 * 2, $result->width());
+        $this->assertEquals(600 * 2, $result->height());
+    }
+
+    public function testScaleDown(): void
+    {
+        $rectangle = new Rectangle(800, 600);
+        $result = $rectangle->scaleDown(height: 1200);
+        $this->assertEquals(800, $result->width());
+        $this->assertEquals(600, $result->height());
+    }
+
+    public function testCover(): void
+    {
+        $rectangle = new Rectangle(800, 600);
+        $result = $rectangle->cover(400, 100);
+        $this->assertEquals(400, $result->width());
+        $this->assertEquals(300, $result->height());
+    }
+
+    public function testContain(): void
+    {
+        $rectangle = new Rectangle(800, 600);
+        $result = $rectangle->contain(1600, 1200);
+        $this->assertEquals(1600, $result->width());
+        $this->assertEquals(1200, $result->height());
+    }
+
+    public function testContainDown(): void
+    {
+        $rectangle = new Rectangle(800, 600);
+        $result = $rectangle->containDown(1600, 1200);
+        $this->assertEquals(800, $result->width());
+        $this->assertEquals(600, $result->height());
+    }
+
+    public function testDebugInfo(): void
+    {
+        $info = (new Rectangle(800, 600))->__debugInfo();
+        $this->assertEquals(800, $info['width']);
+        $this->assertEquals(600, $info['height']);
     }
 }

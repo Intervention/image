7317709586cd5ec8c68402af3f2a2b37f7e3ef4f
Oliver Vogel <oliver@olivervogel.com>
2026-05-15 07:59:48 +0200

    Implement Size::orientation() with enum result

---
+++ src/Orientation.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Intervention\Image;
+
+use Intervention\Image\Interfaces\SizeInterface;
+
+enum Orientation: string
+{
+    case PORTRAIT = 'portrait';
+    case LANDSCAPE = 'landscape';
+    case SQUARE = 'square';
+
+    public static function fromSize(SizeInterface $size): self
+    {
+        return match (true) {
+            $size->isPortrait() => self::PORTRAIT,
+            $size->isLandscape() => self::LANDSCAPE,
+            default => self::SQUARE,
+        };
+    }
+}

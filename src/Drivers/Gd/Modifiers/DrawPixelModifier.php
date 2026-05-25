292d5c55921f7a8b2fba4d3bc63db516110ef4f2
Oliver Vogel <oliver@olivervogel.com>
2026-05-25 09:38:42 +0200

    Refactor for PHP 8.5 return types

--- src/Drivers/Gd/Modifiers/DrawPixelModifier.php
+++ src/Drivers/Gd/Modifiers/DrawPixelModifier.php
@@ -4,9 +4,7 @@ declare(strict_types=1);
 
 namespace Intervention\Image\Drivers\Gd\Modifiers;
 
-use GdImage;
 use Intervention\Image\Exceptions\ColorDecoderException;
-use Intervention\Image\Exceptions\ModifierException;
 use Intervention\Image\Exceptions\StateException;
 use Intervention\Image\Interfaces\ImageInterface;
 use Intervention\Image\Interfaces\SpecializedInterface;
@@ -19,7 +17,6 @@ class DrawPixelModifier extends GenericDrawPixelModifier implements SpecializedI
      *
      * @see ModifierInterface::apply()
      *
-     * @throws ModifierException
      * @throws StateException
      * @throws ColorDecoderException
      */
@@ -28,28 +25,15 @@ class DrawPixelModifier extends GenericDrawPixelModifier implements SpecializedI
         $color = $this->driver()->colorProcessor($image)->export($this->color());
 
         foreach ($image as $frame) {
-            $this->drawPixel($frame->native(), $color);
+            imagealphablending($frame->native(), true);
+            imagesetpixel(
+                $frame->native(),
+                $this->position->x(),
+                $this->position->y(),
+                $color
+            );
         }
 
         return $image;
-    }
-
-    /**
-     * Draw pixel in given color at current position.
-     *
-     * @throws ModifierException
-     */
-    private function drawPixel(GdImage $canvas, int $color): void
-    {
-        $result = imagealphablending($canvas, true) && imagesetpixel(
-            $canvas,
-            $this->position->x(),
-            $this->position->y(),
-            $color
-        );
-
-        if ($result === false) {
-            throw new ModifierException('Failed to apply ' . self::class . ', unable to draw pixel');
-        }
     }
 }
